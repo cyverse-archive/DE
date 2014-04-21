831728d5306c29113d5c8ced4e0919920c32da4c
Sriram Srinivasan <sriram@iplantcollaborative.org>
2014-04-21 14:11:06 -0700

    update stub with new method

--- modules/de-common-module/src/main/java/org/iplantc/de/client/services/stubs/DiskResourceServiceFacadeStub.java
+++ modules/de-common-module/src/main/java/org/iplantc/de/client/services/stubs/DiskResourceServiceFacadeStub.java
@@ -1,7 +1,13 @@
 package org.iplantc.de.client.services.stubs;
 
 import org.iplantc.de.client.models.HasPaths;
-import org.iplantc.de.client.models.diskResources.*;
+import org.iplantc.de.client.models.diskResources.DiskResource;
+import org.iplantc.de.client.models.diskResources.DiskResourceAutoBeanFactory;
+import org.iplantc.de.client.models.diskResources.DiskResourceExistMap;
+import org.iplantc.de.client.models.diskResources.DiskResourceMetadata;
+import org.iplantc.de.client.models.diskResources.DiskResourceStatMap;
+import org.iplantc.de.client.models.diskResources.Folder;
+import org.iplantc.de.client.models.diskResources.RootFolders;
 import org.iplantc.de.client.models.services.DiskResourceMove;
 import org.iplantc.de.client.services.DiskResourceServiceFacade;
 
@@ -231,6 +237,12 @@ public class DiskResourceServiceFacadeStub implements DiskResourceServiceFacade 
 
     @Override
     public void getMetadataTemplate(String templateId, AsyncCallback<String> callback) {
+
+    }
+
+    @Override
+    public void shareWithAnonymous(HasPaths diskResourcePaths, AsyncCallback<String> callback) {
+        // TODO Auto-generated method stub
 
     }
 }
