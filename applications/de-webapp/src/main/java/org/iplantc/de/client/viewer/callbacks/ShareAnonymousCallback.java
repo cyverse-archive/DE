964ca181f50f89b043ec023c8fe410e8a6500ea9
Jonathan Strootman <jstroot@iplantcollaborative.org>
2014-05-15 14:47:32 -0700

    CORE-5553: Added message and context help
    (cherry picked from commit c72b79a3406d16e1ceb46aeb2a4e580a6581c110)

--- applications/de-webapp/src/main/java/org/iplantc/de/client/viewer/callbacks/ShareAnonymousCallback.java
+++ applications/de-webapp/src/main/java/org/iplantc/de/client/viewer/callbacks/ShareAnonymousCallback.java
@@ -5,9 +5,14 @@ import org.iplantc.de.client.models.diskResources.File;
 import org.iplantc.de.client.util.JsonUtil;
 import org.iplantc.de.commons.client.ErrorHandler;
 import org.iplantc.de.commons.client.views.gxt3.dialogs.IPlantDialog;
+import org.iplantc.de.resources.client.IplantResources;
 import org.iplantc.de.resources.client.messages.I18N;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 
@@ -15,9 +20,18 @@ import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.form.FieldLabel;
 import com.sencha.gxt.widget.core.client.form.FormPanel.LabelAlign;
 import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.tips.QuickTip;
+
 
 public class ShareAnonymousCallback implements AsyncCallback<String> {
-    
+
+    interface EnsemblPopupTemplate extends SafeHtmlTemplates {
+        @SafeHtmlTemplates.Template("{0}<img src='{1}' qtip='{2}'></img>")
+        SafeHtml notificationWithContextHelp(SafeHtml label, SafeUri img, String toolTip);
+    }
+
+    private EnsemblPopupTemplate template = GWT.create(EnsemblPopupTemplate.class);
+
     private final IsMaskable container;
     private final File file;
     
@@ -44,13 +58,15 @@ public class ShareAnonymousCallback implements AsyncCallback<String> {
         showShareLink(JsonUtil.getString(paths, file.getPath()));
     }
 
+
     private void showShareLink(String linkId) {
         // Open dialog window with text selected.
         IPlantDialog dlg = new IPlantDialog();
         dlg.setHeadingText(I18N.DISPLAY.sendToEnsemblMenuItem());
         dlg.setHideOnButtonClick(true);
         dlg.setResizable(false);
-        dlg.setSize("535", "130");
+        dlg.setSize("535", "150");
+
         FieldLabel fl = new FieldLabel();
         fl.setHTML("Please vist <a target='_blank' href='http://www.ensembl.org/'>Ensembl</a> and use the following URL to import your bam / vcf / gff file");
         TextField textBox = new TextField();
@@ -59,10 +75,21 @@ public class ShareAnonymousCallback implements AsyncCallback<String> {
         textBox.setValue(linkId);
         fl.setWidget(textBox);
         fl.setLabelAlign(LabelAlign.TOP);
+
         VerticalLayoutContainer container = new VerticalLayoutContainer();
-        dlg.setWidget(container);
         container.add(fl);
         container.add(new Label(I18N.DISPLAY.copyPasteInstructions()));
+
+        // Use a fl to get html
+        FieldLabel notification = new FieldLabel();
+        notification.setLabelSeparator("");
+        notification.setLabelAlign(LabelAlign.TOP);
+        notification.setHTML(template.notificationWithContextHelp(I18N.DISPLAY.sendToEnsemblePopupNote(), IplantResources.RESOURCES.help().getSafeUri(), I18N.HELP.sendToEnsemblUrlHelp()));
+        new QuickTip(notification);
+
+        notification.setWidth(500);
+        container.add(notification);
+        dlg.setWidget(container);
         dlg.setFocusWidget(textBox);
         dlg.show();
         textBox.selectAll();
