45982bd96c732ccf246a021b40f473daceafb1b3
Sriram Srinivasan <sriram@iplantcollaborative.org>
2014-06-13 15:38:29 -0700

    remove unused lib

--- modules/gwt-taglist/src/main/java/com/virilis_software/gwt/taglist/client/TagCreationCodex.java
+++
@@ -1,7 +0,0 @@
-package com.virilis_software.gwt.taglist.client;
-
-import com.virilis_software.gwt.taglist.client.tag.Tag;
-
-public interface TagCreationCodex<T extends Tag<?>> {
-     T createTag( Tag<?> tag );
-}
