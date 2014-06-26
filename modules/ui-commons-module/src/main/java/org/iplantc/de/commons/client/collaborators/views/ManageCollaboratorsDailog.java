4ecf8fffa71a160a04fd7c33767b22867508253c
Sriram Srinivasan <sriram@iplantcollaborative.org>
2014-06-26 10:07:32 -0700

    core-5769 fix collaborator

--- modules/ui-commons-module/src/main/java/org/iplantc/de/commons/client/collaborators/views/ManageCollaboratorsDailog.java
+++ modules/ui-commons-module/src/main/java/org/iplantc/de/commons/client/collaborators/views/ManageCollaboratorsDailog.java
@@ -21,13 +21,53 @@ import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * @author sriram
- *
+ * 
  */
 public class ManageCollaboratorsDailog extends IPlantDialog {
+
+    private final class NameComparator implements Comparator<Collaborator> {
+        @Override
+        public int compare(Collaborator o1, Collaborator o2) {
+
+            if (o1.getFirstName() == null && o2.getFirstName() == null) {
+                return 0;
+            }
+
+            if (o1.getFirstName() == null) {
+                return -1;
+            }
+
+            if (o2.getFirstName() == null) {
+                return 1;
+            }
+
+            return o1.getFirstName().compareTo(o2.getFirstName());
+        }
+    }
+
+    private final class NameCell extends AbstractCell<Collaborator> {
+        @Override
+        public void render(com.google.gwt.cell.client.Cell.Context context,
+                           Collaborator value,
+                           SafeHtmlBuilder sb) {
+            StringBuilder builder = new StringBuilder();
+            if (value.getFirstName() != null && !value.getFirstName().isEmpty()) {
+                builder.append(value.getFirstName());
+                if (value.getLastName() != null && !value.getLastName().isEmpty()) {
+                    builder.append(" " + value.getLastName());
+                }
+                sb.appendEscaped(builder.toString());
+            } else {
+                sb.appendEscaped(value.getUserName());
+            }
+
+        }
+    }
 
     private CheckBoxSelectionModel<Collaborator> checkBoxModel;
     private CollaboratorProperties properties;
@@ -42,8 +82,6 @@ public class ManageCollaboratorsDailog extends IPlantDialog {
         p = new ManageCollaboratorsPresenter(view);
         p.go(this);
     }
-
-
 
     private void initDialog() {
         properties = GWT.create(CollaboratorProperties.class);
@@ -81,31 +119,16 @@ public class ManageCollaboratorsDailog extends IPlantDialog {
         ColumnConfig<Collaborator, Collaborator> colCheckBox = checkBoxModel.getColumn();
         configs.add(colCheckBox);
 
-        ColumnConfig<Collaborator, Collaborator> name = new ColumnConfig<Collaborator, Collaborator>(
-                valueProvider, 150);
+        ColumnConfig<Collaborator, Collaborator> name = new ColumnConfig<Collaborator, Collaborator>(new IdentityValueProvider<Collaborator>("firstname"),
+                                                                                                     150);
         name.setHeader(I18N.DISPLAY.name());
-        name.setCell(new AbstractCell<Collaborator>() {
+        name.setCell(new NameCell());
 
-            @Override
-            public void render(com.google.gwt.cell.client.Cell.Context context, Collaborator value,
-                    SafeHtmlBuilder sb) {
-                StringBuilder builder = new StringBuilder();
-                if (value.getFirstName() != null && !value.getFirstName().isEmpty()) {
-                    builder.append(value.getFirstName());
-                    if (value.getLastName() != null && !value.getLastName().isEmpty()) {
-                        builder.append(" " + value.getLastName());
-                    }
-                    sb.appendEscaped(builder.toString());
-                } else {
-                    sb.appendEscaped(value.getUserName());
-                }
-
-            }
-        });
+        name.setComparator(new NameComparator());
         configs.add(name);
 
-        ColumnConfig<Collaborator, String> ins = new ColumnConfig<Collaborator, String>(
-                properties.institution(), 150);
+        ColumnConfig<Collaborator, String> ins = new ColumnConfig<Collaborator, String>(properties.institution(),
+                                                                                        150);
         ins.setHeader(I18N.DISPLAY.institution());
         configs.add(ins);
         return new ColumnModel<Collaborator>(configs);
