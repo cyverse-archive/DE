bae7ce356699bb465e802be5a5a323a2f6bdc790
Jonathan Strootman <jstroot@iplantcollaborative.org>
2015-04-27 10:41:43 -0700

    Integrated oauth callback controller
    CORE-6624

--- de-lib/src/main/java/org/iplantc/de/shared/exceptions/HttpRedirectException.java
+++ de-lib/src/main/java/org/iplantc/de/shared/exceptions/HttpRedirectException.java
@@ -1,9 +1,11 @@
 package org.iplantc.de.shared.exceptions;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 /**
  * Thrown by the service dispatcher when an HTTP redirect is received.
  */
-public class HttpRedirectException extends HttpException {
+public class HttpRedirectException extends HttpException implements IsSerializable {
 
     private String location;
     public String getLocation() { return location; }
