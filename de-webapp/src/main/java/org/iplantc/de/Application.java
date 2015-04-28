a5cb93a14d7596b3f405a24937a06895b64370fe
Jonathan Strootman <jstroot@iplantcollaborative.org>
2015-04-28 15:30:30 -0700

    Wars now support --version flag
    % java -jar path/to/war --version
    CORE-6624

--- de-webapp/src/main/java/org/iplantc/de/Application.java
+++ de-webapp/src/main/java/org/iplantc/de/Application.java
@@ -12,6 +12,13 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 /**
  * The entry point for the DE webapp.
  *
@@ -26,8 +33,49 @@ public class Application extends AbstractAnnotationConfigDispatcherServletInitia
     private Environment environment;
     final static Logger LOG = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        for (String arg : args) {
+            if (arg.equals("--version")) {
+                Application instance = new Application();
+                // Print manifest and exit
+                instance.printVersion();
+                System.exit(0);
+                return;
+            } else {
+                System.out.println("Unrecognized argument. Try \"--version\"");
+            }
+        }
         SpringApplication.run(Application.class, args);
+
+    }
+
+    private void printVersion() throws IOException {
+        Enumeration<URL> resources = getClass().getClassLoader()
+                                               .getResources("META-INF/MANIFEST.MF");
+        while (resources.hasMoreElements()) {
+            Manifest manifest = new Manifest(resources.nextElement().openStream());
+            Attributes attr = manifest.getMainAttributes();
+
+            // Make sure this is our manifest
+            boolean isCorrectManifest = false;
+            for (Object key : attr.keySet()) {
+                if (key.toString().equals("Start-Class")) {
+                    if (attr.get(key).toString().equals(getClass().getName())) {
+                        isCorrectManifest = true;
+                    }
+                }
+            }
+            if (isCorrectManifest) {
+                StringBuilder sb = new StringBuilder();
+                for (Object key : attr.keySet()) {
+                    sb.append(key)
+                      .append(": ")
+                      .append(attr.get(key).toString())
+                      .append("\n");
+                }
+                System.out.println(sb.toString());
+            }
+        }
     }
 
     @Override
