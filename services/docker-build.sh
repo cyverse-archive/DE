eb25c4a84c9f3dcae874dbfb39b73ac98f3f05a4
Ian McEwen <mian@iplantcollaborative.org>
2015-11-04 14:31:12 -0700

    Add an org.iplantc.de.buildenv.git-ref to the buildenv image and prepare tools/services docker-build.sh files to interpolate the buildenv git commit into templated dockerfiles.

--- services/docker-build.sh
+++ services/docker-build.sh
@@ -6,10 +6,15 @@ VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 GIT_COMMIT=$(git rev-parse HEAD)
 
 docker pull $DOCKER_USER/buildenv:latest
+
+BUILDENV_GIT_COMMIT=$(docker inspect -f '{{ (index .Config.Labels "org.iplantc.de.buildenv.git-ref")}}' $DOCKER_USER/buildenv:latest)
+
 docker run --rm -e "GIT_COMMIT=$GIT_COMMIT" -v $(pwd):/build -w /build $DOCKER_USER/buildenv lein uberjar
 if [ -f Dockerfile.template ]
 then
-  sed -e "s/%%GIT_COMMIT%%/$GIT_COMMIT/g" Dockerfile.template > Dockerfile.$GIT_COMMIT
+  sed -e "s/%%GIT_COMMIT%%/$GIT_COMMIT/g" \
+      -e "s/%%BUILDENV_GIT_COMMIT%%/$BUILDENV_GIT_COMMIT/g" \
+      Dockerfile.template > Dockerfile.$GIT_COMMIT
   docker build --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" -f Dockerfile.$GIT_COMMIT .
   rm Dockerfile.$GIT_COMMIT
 else
