ffd62e8dba932fbba57944dc70b9ede8f3de1953
Ian McEwen <mian@iplantcollaborative.org>
2015-11-05 16:56:12 -0700

    CORE-7221: update jex-events and kifshare docker-build scripts, which are separate and needed updates for labeling

--- services/jex-events/docker-build.sh
+++ services/jex-events/docker-build.sh
@@ -11,16 +11,23 @@ if [ -z "$DOCKER_REPO" ]; then
 fi
 
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
+GIT_COMMIT=$(git rev-parse HEAD)
 
 docker pull $DOCKER_USER/buildenv:latest
+
+BUILDENV_GIT_COMMIT=$(docker inspect -f '{{ (index .Config.Labels "org.iplantc.de.buildenv.git-ref")}}' $DOCKER_USER/buildenv:latest)
+
 docker run --rm  \
-	-e "VERSION=$VERISON" \
-	-e "GIT_COMMIT=$(git rev-parse HEAD)" \
+	-e "VERSION=$VERSION" \
+	-e "GIT_COMMIT=$GIT_COMMIT)" \
 	-e "BUILD_USER=$(whoami)" \
 	-v $(pwd):/jex-events \
 	-w /jex-events \
 	$DOCKER_USER/buildenv:latest \
 	gb build --ldflags "-X main.appver=$VERSION -X main.gitref=$GIT_COMMIT -X main.builtby=$BUILD_USER"
-docker build --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
+docker build --build-arg git_commit=$GIT_COMMIT \
+             --build-arg buildenv_git_commit=$BUILDENV_GIT_COMMIT \
+             --build-arg version=$VERSION \
+             --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
 docker push $DOCKER_USER/$DOCKER_REPO:dev
 docker rmi $DOCKER_USER/$DOCKER_REPO:dev
