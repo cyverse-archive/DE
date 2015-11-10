ce8a784c7d250d199d0f8819454edfab16443b08
Dennis Roberts <dennis@iplantcollaborative.org>
2015-11-09 18:29:59 -0700

    CORE-7248: added code to sharkbait to define the default set of DE folders

--- tools/sharkbait/src/sharkbait/core.clj
+++ tools/sharkbait/src/sharkbait/core.clj
@@ -1,14 +1,60 @@
 (ns sharkbait.core
-  (:import [edu.internet2.middleware.grouper GrouperSession StemFinder SubjectFinder]))
+  (:require [clojure.string :as string])
+  (:import [edu.internet2.middleware.grouper GrouperSession Stem StemFinder SubjectFinder]
+           [edu.internet2.middleware.grouper.misc SaveMode]))
+
+(def ^:private de-username        "de_grouper")
+(def ^:private de-folder          "iplant:de")
+(def ^:private de-users-folder    "iplant:de:users")
+(def ^:private de-apps-folder     "iplant:de:apps")
+(def ^:private de-analyses-folder "iplant:de:analyses")
+
+(def ^:private default-folder-names
+  [de-users-folder
+   de-apps-folder
+   de-analyses-folder])
+
+(def ^:private find-subject
+  "Finds a subject with the given ID. This function is memoized because it appears that searching
+   for a subject more than once can cause null pointer exceptions."
+  (memoize (fn [subject-id] (SubjectFinder/findById subject-id true))))
 
 (defn- create-grouper-session
   "Creates a Grouper session using the root Grouper account."
+  ([]
+     (GrouperSession/start (SubjectFinder/findRootSubject)))
+  ([subject-id]
+     (GrouperSession/start (find-subject subject-id))))
+
+(defn- find-folder
+  "Finds a folder in Grouper, creating it and any parent folders if necessary."
+  [session name]
+  (or (StemFinder/findByName session name false)
+      (Stem/saveStem session nil nil name nil nil SaveMode/INSERT true)))
+
+(defn- perform-root-actions
+  "Performs the actions that require superuser privileges."
   []
-  (GrouperSession/start (SubjectFinder/findRootSubject)))
+  (let [session (create-grouper-session)]
+    (try
+      (-> (find-folder session de-folder)
+          (.grantPrivs (find-subject de-username) true false false false false))
+      (finally (GrouperSession/stopQuietly session)))))
+
+;; TODO: implement me
+(defn- create-app-permission-def
+  [session])
+
+(defn- perform-de-user-actions
+  "Performs the actions that do not require superuser privileges."
+  []
+  (let [session (create-grouper-session de-username)]
+    (try
+      (dorun (map (partial find-folder session) default-folder-names))
+      (create-app-permisison-def session)
+      (finally (GrouperSession/stopQuietly session)))))
 
 (defn -main
   [& args]
-  (let [session (create-grouper-session)]
-    (try
-      (println (StemFinder/findByName session, "tmp:iplant", true))
-      (finally (GrouperSession/stopQuietly session)))))
+  (perform-root-actions)
+  (perform-de-user-actions))
