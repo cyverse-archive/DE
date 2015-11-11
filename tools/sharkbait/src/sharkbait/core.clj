2b206c47a2089f7d88b05b3b4a258ca64798ca61
Dennis Roberts <dennis@iplantcollaborative.org>
2015-11-10 18:35:42 -0700

    CORE-7248: updated sharkbait so that it creates the permission definitions that we need

--- tools/sharkbait/src/sharkbait/core.clj
+++ tools/sharkbait/src/sharkbait/core.clj
@@ -1,6 +1,7 @@
 (ns sharkbait.core
   (:require [clojure.string :as string])
   (:import [edu.internet2.middleware.grouper GrouperSession Stem StemFinder SubjectFinder]
+           [edu.internet2.middleware.grouper.attr AttributeDefSave AttributeDefType]
            [edu.internet2.middleware.grouper.misc SaveMode]))
 
 (def ^:private de-username        "de_grouper")
@@ -41,9 +42,34 @@
           (.grantPrivs (find-subject de-username) true false false false false))
       (finally (GrouperSession/stopQuietly session)))))
 
-;; TODO: implement me
-(defn- create-app-permission-def
-  [session])
+(defn- add-implied-action
+  "Creates a relationship between two actions so that one action implies another."
+  [action-delegate implying-action-name implied-action-name]
+  (let [implying-action (.findAction action-delegate implying-action-name true)
+        implied-action  (.findAction action-delegate implied-action-name true)]
+    (-> (.getAttributeAssignActionSetDelegate implying-action)
+        (.addToAttributeAssignActionSet implied-action))))
+
+(defn- set-permission-actions
+  "Sets the allowed actions for a permission definition."
+  [permission-def]
+  (let [delegate (.getAttributeDefActionDelegate permission-def)]
+    (.configureActionList delegate ["read" "write" "own"])
+    (add-implied-action delegate "own" "write")
+    (add-implied-action delegate "write" "read"))
+  (.store permission-def))
+
+(defn- create-permission-def
+  "Creates a permission definition."
+  [session permission-name folder]
+  (-> (AttributeDefSave. session)
+      (.assignAttributeDefType AttributeDefType/perm)
+      (.assignName (str folder ":" permission-name))
+      (.assignSaveMode SaveMode/INSERT_OR_UPDATE)
+      (.assignToGroup true)
+      (.assignToEffMembership true)
+      (.save)
+      (set-permission-actions)))
 
 (defn- perform-de-user-actions
   "Performs the actions that do not require superuser privileges."
@@ -51,7 +77,8 @@
   (let [session (create-grouper-session de-username)]
     (try
       (dorun (map (partial find-folder session) default-folder-names))
-      (create-app-permisison-def session)
+      (create-permission-def session "appPermissionDef" de-apps-folder)
+      (create-permission-def session "analysisPermissionDef" de-analyses-folder)
       (finally (GrouperSession/stopQuietly session)))))
 
 (defn -main
