769e5afda6e50b879507f28d986eb5cfdbf44eea
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-11-12 10:46:08 -0700

    Added the metadata and notifications databases to de-db-loader.

--- docker/de-db-loader/setup-dev-database.sh
+++ docker/de-db-loader/setup-dev-database.sh
@@ -4,4 +4,8 @@ set -x
 set -e
 
 #psql -h 127.0.0.1 -U $POSTGRES_USER -d postgres -c "CREATE DATABASE de WITH OWNER $POSTGRES_USER;"
-java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d de /database.tar.gz
+java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d de -f /database.tar.gz
+psql -h 127.0.0.1 -U $POSTGRES_USER -d postgres -c "CREATE DATABASE metadata WITH OWNER $POSTGRES_USER;"
+java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d metadata -f /metadata-db.tar.gz
+psql -h 127.0.0.1 -U $POSTGRES_USER -d postgres -c "CREATE DATABASE notifications WITH OWNER $POSTGRES_USER;"
+java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d notifications -f /notification-db.tar.gz
