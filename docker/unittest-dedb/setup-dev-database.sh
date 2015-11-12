dada32cf3d5d06cccea83cff20a8cfd3ffd31a57
John Wregglesworth <wregglej@iplantcollaborative.org>
2015-11-12 12:48:24 -0700

    Removed commented out line.

--- docker/unittest-dedb/setup-dev-database.sh
+++ docker/unittest-dedb/setup-dev-database.sh
@@ -3,7 +3,6 @@
 set -x
 set -e
 
-#psql -h 127.0.0.1 -U $POSTGRES_USER -d postgres -c "CREATE DATABASE de WITH OWNER $POSTGRES_USER;"
 java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d de -f /database.tar.gz
 psql -h 127.0.0.1 -U $POSTGRES_USER -d postgres -c "CREATE DATABASE metadata WITH OWNER $POSTGRES_USER;"
 java -jar /facepalm-standalone.jar -m init -A $POSTGRES_USER -U $POSTGRES_USER -h 127.0.0.1 -d metadata -f /metadata-db.tar.gz
