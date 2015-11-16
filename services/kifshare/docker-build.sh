c9e5018987b99f4dc6f57383225e2f031fbcf2ec
Ian McEwen <mian@iplantcollaborative.org>
2015-11-16 10:33:27 -0700

    CORE-7222: Remove erroneous close-paren from kifshare build script

--- services/kifshare/docker-build.sh
+++ services/kifshare/docker-build.sh
@@ -9,7 +9,7 @@ docker pull $DOCKER_USER/buildenv:latest
 
 BUILDENV_GIT_COMMIT=$(docker inspect -f '{{ (index .Config.Labels "org.iplantc.de.buildenv.git-ref")}}' $DOCKER_USER/buildenv:latest)
 
-docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$GIT_COMMIT)" -v $(pwd):/build -w /build $DOCKER_USER/buildenv ./intra-container-build.sh
+docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$GIT_COMMIT" -v $(pwd):/build -w /build $DOCKER_USER/buildenv ./intra-container-build.sh
 
 docker build --build-arg git_commit=$GIT_COMMIT \
              --build-arg buildenv_git_commit=$BUILDENV_GIT_COMMIT \
