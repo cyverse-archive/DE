4f64fb39698d7aff9d188b3fecdc4e8f07b1b89b
Ian McEwen <mian@iplantcollaborative.org>
2015-11-17 18:35:36 -0700

    Also use c3p0 0.9.5.1 for clj-icat-direct.

--- libs/clj-icat-direct/project.clj
+++ libs/clj-icat-direct/project.clj
@@ -4,5 +4,7 @@
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [korma "0.4.2"]
+                 [com.mchange/c3p0 "0.9.5.1"]
+                 [korma "0.4.2"
+                  :exclusions [c3p0]]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]])
