c638aebb1a998fd804b7867c2b4ab0c75c457a9b
Tony Edgin <tedgin@iplantcollaborative.org>
2015-11-19 12:54:46 -0700

    CORE-6924 Upgraded to Jargon 4.0.2.4-RELEASE

--- libs/clj-jargon/project.clj
+++ libs/clj-jargon/project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
-                 [org.irods.jargon/jargon-core "4.0.2.4-RC1"
+                 [org.irods.jargon/jargon-core "4.0.2.4-RELEASE"
                   :exclusions [[org.jglobus/JGlobus-Core]
                                [org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.irods.jargon/jargon-data-utils "4.0.2.4-RC1"
+                 [org.irods.jargon/jargon-data-utils "4.0.2.4-RELEASE"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
-                 [org.irods.jargon/jargon-ticket "4.0.2.4-RC1"
+                 [org.irods.jargon/jargon-ticket "4.0.2.4-RELEASE"
                   :exclusions [[org.slf4j/slf4j-api]
                                [org.slf4j/slf4j-log4j12]]]
                  [slingshot "0.12.2"]
