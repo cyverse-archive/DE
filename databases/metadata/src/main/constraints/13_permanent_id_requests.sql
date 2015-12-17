9275fb67828d7c684e7da0164578cdc50edd9102
Paul Sarando <psarando@iplantcollaborative.org>
2015-12-16 17:51:08 -0700

    CORE-7372 Update permanent_id_request tables.
    Added unique constraint to permanent_id_requests table.
    Added delete cascade to permanent_id_request_statuses table.

--- databases/metadata/src/main/constraints/13_permanent_id_requests.sql
+++ databases/metadata/src/main/constraints/13_permanent_id_requests.sql
@@ -11,3 +11,9 @@ ALTER TABLE ONLY permanent_id_requests
     ADD CONSTRAINT permanent_id_requests_type_fkey
     FOREIGN KEY (type)
     REFERENCES permanent_id_request_types(id);
+
+--
+-- All Permanent ID requests should be for unique target/type combinations.
+--
+CREATE UNIQUE INDEX permanent_id_requests_unique
+    ON permanent_id_requests(target_id, type);
