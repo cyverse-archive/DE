b2081e35a6ec04d18d5967144a262521811cbee4
Ian McEwen <mian@iplantcollaborative.org>
2016-01-19 13:23:30 -0700

    Modify apps' test.sh to better query for the de-db container, as well as using a more unique name for that container.

--- services/apps/test.sh
+++ services/apps/test.sh
@@ -10,16 +10,17 @@ if [ -z $CMD ]; then
 fi
 
 OS=$(uname)
+DBCONTAINER=apps-de-db
 
-if [ $(docker ps | grep de-db | wc -l) -gt 0 ]; then
-    docker kill de-db
+if [ $(docker ps -qf "name=$DBCONTAINER" wc -l) -gt 0 ]; then
+    docker kill $DBCONTAINER
 fi
 
-if [ $(docker ps -a | grep de-db | wc -l) -gt 0 ]; then
-    docker rm de-db
+if [ $(docker ps -aqf "name=$DBCONTAINER" | wc -l) -gt 0 ]; then
+    docker rm $DBCONTAINER
 fi
 
-docker run --name de-db -e POSTGRES_PASSWORD=notprod -d -p 35432:5432 discoenv/de-db
+docker run --name $DBCONTAINER -e POSTGRES_PASSWORD=notprod -d -p 35432:5432 discoenv/de-db
 sleep 5
-docker run --rm --link de-db:postgres discoenv/de-db-loader:dev
-docker run -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link de-db:postgres clojure lein $CMD
+docker run --rm --link $DBCONTAINER:postgres discoenv/de-db-loader:dev
+docker run -i -t --rm -v $(pwd):/build -v ~/.m2:/root/.m2 -w /build --link $DBCONTAINER:postgres clojure lein $CMD
