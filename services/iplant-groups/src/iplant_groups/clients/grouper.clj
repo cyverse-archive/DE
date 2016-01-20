e8d4d6d1987ac5c73b9d655437ada391bd2f449e
Dennis Roberts <dennis@iplantcollaborative.org>
2016-01-20 16:25:56 -0700

    CORE-7444: modify GET /attributes/permissions so that it allows attribute definition names and IDs to be included in the same request

--- services/iplant-groups/src/iplant_groups/clients/grouper.clj
+++ services/iplant-groups/src/iplant_groups/clients/grouper.clj
@@ -527,8 +527,8 @@
 
 (defn- format-attribute-def-name-lookup
   [{:keys [attribute_def_name_ids attribute_def_names]}]
-  (cond (seq attribute_def_name_ids) (mapv (partial hash-map :uuid) attribute_def_name_ids)
-        (seq attribute_def_names)    (mapv (partial hash-map :name) attribute_def_names)))
+  (concat (mapv (partial hash-map :uuid) attribute_def_name_ids)
+          (mapv (partial hash-map :name) attribute_def_names)))
 
 (defn- format-role-lookup
   [{:keys [role_id role]}]
