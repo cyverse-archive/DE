6f71ab88dc8590184318086ac44d5e9eb07e37ba
Dennis Roberts <dennis@iplantcollaborative.org>
2016-01-25 14:29:55 -0700

    CORE-7441: renamed some schema definitions in apps for clarification

--- services/apps/src/apps/routes/domain/permission.clj
+++ services/apps/src/apps/routes/domain/permission.clj
@@ -6,13 +6,13 @@
 (defschema AppIdList
   {:apps (describe [NonBlankString] "A List of app identifiers")})
 
-(defschema UserPermission
+(defschema UserPermissionListElement
   {:user       (describe NonBlankString "The user ID")
    :permission (describe (enum "read" "write" "own" "") "The permission level assigned to the user")})
 
-(defschema AppPermissions
+(defschema AppPermissionListElement
   {:id          (describe NonBlankString "The app ID")
-   :permissions (describe [UserPermission] "The list of user permissions for the app")})
+   :permissions (describe [UserPermissionListElement] "The list of user permissions for the app")})
 
 (defschema AppPermissionListing
-  {:apps (describe [AppPermissions] "The list of app permissions")})
+  {:apps (describe [AppPermissionListElement] "The list of app permissions")})
