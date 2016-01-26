e8b4627d1d7ab8e5ac085876fc2f18e075bc3a9a
Paul Sarando <psarando@iplantcollaborative.org>
2016-01-26 12:35:31 -0700

    CORE-7372 Fix typo.

--- databases/metadata/src/main/constraints/12_permanent_id_request_types.sql
+++ databases/metadata/src/main/constraints/12_permanent_id_request_types.sql
@@ -1,7 +1,7 @@
 SET search_path = public, pg_catalog;
 
 --
--- permanent_id_requests table primary key.
+-- permanent_id_request_types table primary key.
 --
 ALTER TABLE ONLY permanent_id_request_types
     ADD CONSTRAINT permanent_id_request_types_pkey
