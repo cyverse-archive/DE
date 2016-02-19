f2927d61299ad4e59505fbdb0947e33d4b6e61bf
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-02-19 13:47:52 -0700

    Fixed test.sh to exit when tests are complete

--- services/jobservices/test.sh
+++ services/jobservices/test.sh
@@ -3,4 +3,7 @@
 set -e
 set -x
 
-docker-compose -f test.yml up --force-recreate
+docker-compose -f test.yml up -d --force-recreate jexdb rabbit
+docker-compose -f test.yml up --force-recreate test
+docker-compose -f test.yml stop
+docker-compose -f test.yml rm -v -f
