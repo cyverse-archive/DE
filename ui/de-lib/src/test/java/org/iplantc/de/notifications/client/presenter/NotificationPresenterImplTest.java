615028ed491f5f47411333713772f6344a07a848
Ashley Ramsey <aramsey@cyverse.org>
2016-02-22 11:20:16 -0700

    CORE-6341 Refactor Notifications tests

--- ui/de-lib/src/test/java/org/iplantc/de/notifications/client/presenter/NotificationPresenterImplTest.java
+++ ui/de-lib/src/test/java/org/iplantc/de/notifications/client/presenter/NotificationPresenterImplTest.java
@@ -11,6 +11,12 @@ import org.iplantc.de.client.models.notifications.NotificationCategory;
 import org.iplantc.de.client.models.notifications.NotificationMessage;
 import org.iplantc.de.client.services.MessageServiceFacade;
 import org.iplantc.de.notifications.client.events.DeleteNotificationsUpdateEvent;
+import org.iplantc.de.notifications.client.events.NotificationGridRefreshEvent;
+import org.iplantc.de.notifications.client.events.NotificationSelectionEvent;
+import org.iplantc.de.notifications.client.events.NotificationToolbarDeleteAllClickedEvent;
+import org.iplantc.de.notifications.client.events.NotificationToolbarDeleteClickedEvent;
+import org.iplantc.de.notifications.client.gin.factory.NotificationViewFactory;
+import org.iplantc.de.notifications.client.model.NotificationMessageProperties;
 import org.iplantc.de.notifications.client.views.NotificationToolbarView;
 import org.iplantc.de.notifications.client.views.NotificationView;
 
@@ -37,7 +43,10 @@ import java.util.List;
 @RunWith(GxtMockitoTestRunner.class)
 public class NotificationPresenterImplTest {
 
+    @Mock NotificationViewFactory viewFactoryMock;
+    @Mock NotificationView.NotificationViewAppearance appearanceMock;
     @Mock NotificationView viewMock;
+    @Mock NotificationMessageProperties messagePropertiesMock;
     @Mock MessageServiceFacade messageServiceFacadeMock;
     @Mock NotificationToolbarView toolbarViewMock;
     @Mock EventBus eventBusMock;
@@ -53,59 +62,75 @@ public class NotificationPresenterImplTest {
 
     @Before
     public void setUp() {
+        when(viewFactoryMock.create(listStoreMock)).thenReturn(viewMock);
         when(currentCategoryMock.toString()).thenReturn("sample");
         when(viewMock.getCurrentLoadConfig()).thenReturn(mock(FilterPagingLoadConfig.class));
         when(notificationMessageMock.getId()).thenReturn("id");
 
-        uut = new NotificationPresenterImpl(viewMock);
-
+        uut = new NotificationPresenterImpl(viewFactoryMock,
+                                            appearanceMock,
+                                            toolbarViewMock,
+                                            messagePropertiesMock) {
+            @Override
+            ListStore<NotificationMessage> createListStore(NotificationMessageProperties messageProperties) {
+                return listStoreMock;
+            }
+        };
         uut.currentCategory = currentCategoryMock;
         uut.messageServiceFacade = messageServiceFacadeMock;
-        uut.toolbar = toolbarViewMock;
         uut.eventBus = eventBusMock;
     }
 
     @Test
     public void testOnNotificationGridRefresh_emptyListStore() {
+        NotificationGridRefreshEvent eventMock = mock(NotificationGridRefreshEvent.class);
         when(listStoreMock.size()).thenReturn(0);
-        when(viewMock.getListStore()).thenReturn(listStoreMock);
 
-        uut.onGridRefresh();
+        uut.onNotificationGridRefresh(eventMock);
         verify(toolbarViewMock).setDeleteAllButtonEnabled(eq(false));
     }
 
     @Test
     public void testOnNotificationGridRefresh_nonEmptyListStore() {
+        NotificationGridRefreshEvent eventMock = mock(NotificationGridRefreshEvent.class);
         when(listStoreMock.size()).thenReturn(5);
-        when(viewMock.getListStore()).thenReturn(listStoreMock);
 
-        uut.onGridRefresh();
+        uut.onNotificationGridRefresh(eventMock);
+
         verify(toolbarViewMock).setDeleteAllButtonEnabled(eq(true));
     }
 
     @Test
     public void testOnNotificationSelection_emptyListStore() {
+        NotificationSelectionEvent eventMock = mock(NotificationSelectionEvent.class);
+        when(eventMock.getNotifications()).thenReturn(listMock);
         when(listMock.size()).thenReturn(0);
 
-        uut.onNotificationSelection(listMock);
+        uut.onNotificationSelection(eventMock);
+
         verify(toolbarViewMock).setDeleteButtonEnabled(eq(false));
     }
 
     @Test
     public void testOnNotificationSelection_nonEmptyListStore() {
+        NotificationSelectionEvent eventMock = mock(NotificationSelectionEvent.class);
+        when(eventMock.getNotifications()).thenReturn(listMock);
         when(listMock.size()).thenReturn(5);
 
-        uut.onNotificationSelection(listMock);
+        uut.onNotificationSelection(eventMock);
+
         verify(toolbarViewMock).setDeleteButtonEnabled(eq(true));
     }
 
 
     @Test
     public void testOnNotificationToolbarDeleteAllClicked() {
-        uut.onDeleteAllClicked();
+        NotificationToolbarDeleteAllClickedEvent eventMock = mock(NotificationToolbarDeleteAllClickedEvent.class);
+        uut.onNotificationToolbarDeleteAllClicked(eventMock);
 
         verify(viewMock).mask();
         verify(messageServiceFacadeMock).deleteAll(eq(currentCategoryMock), asyncCallbackStringCaptor.capture());
+
         AsyncCallback<String> asyncCallback = asyncCallbackStringCaptor.getValue();
 
         asyncCallback.onSuccess("result");
@@ -117,6 +142,8 @@ public class NotificationPresenterImplTest {
 
     @Test
     public void testOnNotificationToolbarDeleteClicked() {
+        NotificationToolbarDeleteClickedEvent eventMock = mock(NotificationToolbarDeleteClickedEvent.class);
+
         when(listMock.isEmpty()).thenReturn(false);
         when(listMock.size()).thenReturn(1);
         when(iteratorMock.hasNext()).thenReturn(true, false);
@@ -124,7 +151,8 @@ public class NotificationPresenterImplTest {
         when(listMock.iterator()).thenReturn(iteratorMock);
         when(viewMock.getSelectedItems()).thenReturn(listMock);
 
-        uut.onDeleteClicked();
+        uut.onNotificationToolbarDeleteClicked(eventMock);
+
 
         verify(messageServiceFacadeMock).deleteMessages(isA(JSONObject.class), asyncCallbackStringCaptor.capture());
 
