e19025825360816a4fadc274c2cb0805bb324885
Sriram Srinivasan <sriram@iplantcollaborative.org>
2016-02-23 11:12:34 -0700

    CORE-7512 fix tests.

--- ui/de-lib/src/test/java/org/iplantc/de/admin/desktop/client/permIdRequest/views/UpdatePermanentIdRequestDialogTest.java
+++ ui/de-lib/src/test/java/org/iplantc/de/admin/desktop/client/permIdRequest/views/UpdatePermanentIdRequestDialogTest.java
@@ -1,22 +1,23 @@
 package org.iplantc.de.admin.desktop.client.permIdRequest.views;
 
-import org.iplantc.de.admin.desktop.client.permIdRequest.views.PermanentIdRequestView.PermanentIdRequestViewAppearance;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.iplantc.de.client.models.UserBootstrap;
 import org.iplantc.de.client.models.identifiers.PermanentIdRequest;
 import org.iplantc.de.client.models.identifiers.PermanentIdRequestAutoBeanFactory;
+import org.iplantc.de.client.models.identifiers.PermanentIdRequestDetails;
 import org.iplantc.de.client.models.identifiers.PermanentIdRequestStatus;
 import org.iplantc.de.client.models.identifiers.PermanentIdRequestUpdate;
+import org.iplantc.de.commons.client.widgets.IPlantAnchor;
 
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwtmockito.GwtMockitoTestRunner;
 import com.google.gwtmockito.GxtMockitoTestRunner;
 import com.google.web.bindery.autobean.shared.AutoBean;
 
-import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.form.SimpleComboBox;
 import com.sencha.gxt.widget.core.client.form.TextArea;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,7 +28,10 @@ import org.mockito.Mock;
 public class UpdatePermanentIdRequestDialogTest {
 
     @Mock
-    Label mockCurrentStatusLabel;
+    IPlantAnchor mockCurrentStatusLabel;
+
+    @Mock
+    Label mockUserEmail;
     
     @Mock
     SimpleComboBox<PermanentIdRequestStatus> mockStatusCombo;
@@ -50,17 +54,25 @@ public class UpdatePermanentIdRequestDialogTest {
     @Mock
     AutoBean<PermanentIdRequestUpdate> mockAutoBeanStatus;
 
+    @Mock
+    PermanentIdRequestDetails mockDetails;
+
+    @Mock
+    UserBootstrap mockStrap;
+
     private UpdatePermanentIdRequestDialog dialog;
 
     @Before
     public void setUp() {
-        dialog = new UpdatePermanentIdRequestDialog(mockRequest,
-                                                    mockPresenter,
+       when(mockDetails.getRequestor()).thenReturn(mockStrap);
+        when(mockStrap.getEmail()).thenReturn("foo@bar.com");
+        dialog = new UpdatePermanentIdRequestDialog(PermanentIdRequestStatus.Submitted.toString(),
+                                                    mockDetails,
                                                     mockPrfactory);
         dialog.commentsEditor = mockCommentsEditor;
         dialog.currentStatusLabel = mockCurrentStatusLabel;
         dialog.statusCombo = mockStatusCombo;
-
+        dialog.userEmail = mockUserEmail;
 
     }
 
@@ -69,6 +81,9 @@ public class UpdatePermanentIdRequestDialogTest {
         when(mockStatusCombo.getCurrentValue()).thenReturn(PermanentIdRequestStatus.Approved);
         when(mockCommentsEditor.getValue()).thenReturn("testing");
         when(mockCurrentStatusLabel.getText()).thenReturn(PermanentIdRequestStatus.Submitted.toString());
+        when(mockUserEmail.getText()).thenReturn("foo@bar.com");
+
+
         when(mockPrfactory.getStatus()).thenReturn(mockAutoBeanStatus);
         when(mockAutoBeanStatus.as()).thenReturn(mockStatusUpdate);
         final PermanentIdRequestUpdate pru = mock(PermanentIdRequestUpdate.class);
