47f66cb8b162f9a86b6855da4153b79c4bec5c00
Ashley Ramsey <aramsey@cyverse.org>
2016-03-03 17:50:44 -0700

    CORE-6617 Add staticId to Pipeline AppOrder Grid

---
+++ ui/de-lib/src/main/java/org/iplantc/de/pipelines/shared/Pipelines.java
@@ -0,0 +1,14 @@
+package org.iplantc.de.pipelines.shared;
+
+/**
+ * @author aramsey
+ */
+public interface Pipelines {
+
+    interface Ids {
+        String WORKFLOW_VIEW = ".workflowView";
+
+        String APP_ORDER = ".appOrder";
+        String APP_ORDER_GRID = ".grid";
+    }
+}
