503b9477415d04a1a59223fd136ab2218534f4d1
Dennis Roberts <dennis@iplantcollaborative.org>
2016-03-14 17:17:48 -0700

    CORE-7563: made it easier to visually determine when a test failed or succeeded

--- services/apps/test.sh
+++ services/apps/test.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 set -e
-set -x
+
+function error_exit() {
+    echo
+    echo "TEST FAILED: $@" 1>&2
+    exit 1
+}
 
 CMD=$1
 
@@ -20,12 +25,33 @@ if [ $(docker ps -aqf "name=$DBCONTAINER" | wc -l) -gt 0 ]; then
     docker rm $DBCONTAINER
 fi
 
-docker pull discoenv/buildenv
-docker run --rm -v $(pwd):/build -w /build discoenv/buildenv lein eastwood '{:exclude-namespaces [apps.protocols :test-paths] :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}'
+# Pull the build environment.
+docker pull discoenv/buildenv || error_exit 'unable to pull the build environment image'
 
-docker pull discoenv/de-db
-docker run --name $DBCONTAINER -e POSTGRES_PASSWORD=notprod -d -p 35432:5432 discoenv/de-db
+# Check for syntax errors.
+docker run --rm -v $(pwd):/build -w /build discoenv/buildenv lein eastwood '{:exclude-namespaces [apps.protocols :test-paths] :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}' \
+    || error_exit 'lint errors were found'
+
+# Pull the DE database image.
+docker pull discoenv/de-db || error_exit 'unable to pull the DE database image'
+
+# Start the DE database container.
+docker run --name $DBCONTAINER -e POSTGRES_PASSWORD=notprod -d -p 35432:5432 discoenv/de-db \
+    || error_exit 'unable to start the DE database container'
+
+# Wait for the DE database container to start up.
 sleep 10
-docker pull discoenv/de-db-loader:dev
-docker run --rm --link $DBCONTAINER:postgres discoenv/de-db-loader:dev
-docker run --rm -v $(pwd):/build -w /build --link $DBCONTAINER:postgres discoenv/buildenv lein $CMD
+
+# Pull the DE database loader.
+docker pull discoenv/de-db-loader:dev || error_exit 'unable to pull the DE database loader image'
+
+# Run the DE database loader.
+docker run --rm --link $DBCONTAINER:postgres discoenv/de-db-loader:dev \
+    || error_exit 'unable to run the DE database loader'
+
+# Run the tests.
+docker run --rm -v $(pwd):/build -w /build --link $DBCONTAINER:postgres discoenv/buildenv lein $CMD \
+    || error_exit 'there were unit test failures'
+
+# Display a success message.
+echo "TEST SUCCEEDED" 1>&2
