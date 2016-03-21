86a7c7084eb95e09811355482b7c0c4b996c9f94
Ian McEwen <mian@iplantcollaborative.org>
2016-03-21 11:41:17 -0700

    Update iplant-email tests for automatic running.

---
+++ services/iplant-email/test/iplant_email/core_test.clj
@@ -0,0 +1,7 @@
+(ns iplant-email.core-test
+  (:use [iplant-email.core])
+  (:use [clojure.test]))
+
+(deftest silly-test
+  (testing "This test is totally fake, but it'll pass!"
+    (is (= 1 1))))
--- services/iplant-email/test/iplant_email/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns iplant-email.test.core
-  (:use [iplant-email.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
