f2f5388cc25b2445ce418621fa57dd1f017a1d8b
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-04-04 13:12:23 -0700

    Separated building the binaries from the docker image builds for templeton and jobservices

--- golang/build.sh
+++ golang/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -x
 set -e
 
@@ -6,17 +7,11 @@ if [ -z "$DOCKER_USER" ]; then
 	DOCKER_USER=discoenv
 fi
 
-if [ -z "$DOCKER_REPO" ]; then
-	DOCKER_REPO=jobservices
-fi
-
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 GIT_COMMIT=$(git rev-parse HEAD)
 BUILD_USER=$(whoami)
 
 docker pull $DOCKER_USER/buildenv:latest
-
-BUILDENV_GIT_COMMIT=$(docker inspect -f '{{ (index .Config.Labels "org.iplantc.de.buildenv.git-ref")}}' $DOCKER_USER/buildenv:latest)
 
 if [ -d ./bin ]; then
 	docker run --rm -v $(pwd):/work -w /work $DOCKER_USER/buildenv rm -r bin
@@ -29,15 +24,7 @@ docker run --rm \
 	gb vendor restore
 
 docker run --rm \
-	-v $(pwd):/jobservices \
-	-w /jobservices \
+	-v $(pwd):/build \
+	-w /build \
 	$DOCKER_USER/buildenv:latest \
 	gb build -f -F --ldflags "-X main.appver=$VERSION -X main.gitref=$GIT_COMMIT -X main.builtby=$BUILD_USER"
-
-docker build -f jobservices.docker \
-             --build-arg git_commit=$GIT_COMMIT \
-             --build-arg buildenv_git_commit=$BUILDENV_GIT_COMMIT \
-             --build-arg version=$VERSION \
-             --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
-docker push $DOCKER_USER/$DOCKER_REPO:dev
-docker rmi $DOCKER_USER/$DOCKER_REPO:dev
