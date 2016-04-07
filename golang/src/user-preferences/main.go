3f937e0352ff594f6e90e0f6b816aef688781417
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-04-07 13:13:07 -0700

    Initial commit of the new user-preferences

---
+++ golang/src/user-preferences/main.go
@@ -0,0 +1,393 @@
+package main
+
+import (
+	"configurate"
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"logcabin"
+	"net/http"
+	"os"
+	"queries"
+	"strings"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+var (
+	version = flag.Bool("version", false, "Print the version information")
+	cfgPath = flag.String("config", "/etc/iplant/de/jobservices.yml", "The path to the config file")
+	port    = flag.String("port", "60000", "The port number to listen on")
+	gitref  string
+	appver  string
+	builtby string
+)
+
+func init() {
+	flag.Parse()
+}
+
+// AppVersion prints the version information to stdout
+func AppVersion() {
+	if appver != "" {
+		fmt.Printf("App-Version: %s\n", appver)
+	}
+	if gitref != "" {
+		fmt.Printf("Git-Ref: %s\n", gitref)
+	}
+	if builtby != "" {
+		fmt.Printf("Built-By: %s\n", builtby)
+	}
+}
+
+// App defines the interface for a user-preferences application
+type App interface {
+	Greeting(http.ResponseWriter, *http.Request)
+	GetRequest(http.ResponseWriter, *http.Request)
+	PutRequest(http.ResponseWriter, *http.Request)
+	PostRequest(http.ResponseWriter, *http.Request)
+	DeleteRequest(http.ResponseWriter, *http.Request)
+}
+
+// UserPreferencesRecord represents a user preferences stored in the database
+type UserPreferencesRecord struct {
+	ID          string
+	Preferences string
+	UserID      string
+}
+
+// convert makes sure that the JSON has the correct format. "wrap" tells convert
+// whether to wrap the object in a map with "session" as the key.
+func convert(record *UserPreferencesRecord, wrap bool) (map[string]interface{}, error) {
+	var values map[string]interface{}
+	if record.Preferences != "" {
+		if err := json.Unmarshal([]byte(record.Preferences), &values); err != nil {
+			return nil, err
+		}
+	}
+	// We don't want the return value wrapped in a session object, so unwrap it
+	// if it is wrapped.
+	if !wrap {
+		if _, ok := values["preferences"]; ok {
+			return values["preferences"].(map[string]interface{}), nil
+		}
+		return values, nil
+	}
+	// We do want the return value wrapped in a session object, so wrap it if it
+	// isn't already.
+	if _, ok := values["preferences"]; !ok {
+		newmap := make(map[string]interface{})
+		newmap["preferences"] = values
+		return newmap, nil
+	}
+	return values, nil
+}
+
+// UserPreferencesApp is an implementation of the App interface created to manage
+// user preferences.
+type UserPreferencesApp struct {
+	db *sql.DB
+}
+
+// New returns a new *UserPreferencesApp
+func New(db *sql.DB) *UserPreferencesApp {
+	return &UserPreferencesApp{
+		db: db,
+	}
+}
+
+// hasSessions returns whether or not the given user has preferences already.
+func (u *UserPreferencesApp) hasPreferences(username string) (bool, error) {
+	query := `SELECT COUNT(p.*)
+              FROM user_preferences p,
+                   users u
+             WHERE p.user_id = u.id
+               AND u.username = $1`
+	var count int64
+	if err := u.db.QueryRow(query, username).Scan(&count); err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
+
+// getSessions returns a []UserPreferencesRecord of all of the preferences associated
+// with the provided username.
+func (u *UserPreferencesApp) getPreferences(username string) ([]UserPreferencesRecord, error) {
+	query := `SELECT p.id AS id,
+                   p.user_id AS user_id,
+                   p.preferences AS preferences
+              FROM user_preferences p,
+                   users u
+             WHERE p.user_id = u.id
+               AND u.username = $1`
+	rows, err := u.db.Query(query, username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var prefs []UserPreferencesRecord
+	for rows.Next() {
+		var pref UserPreferencesRecord
+		if err := rows.Scan(&pref.ID, &pref.UserID, &pref.Preferences); err != nil {
+			return nil, err
+		}
+		prefs = append(prefs, pref)
+	}
+	if err := rows.Err(); err != nil {
+		return prefs, err
+	}
+	return prefs, nil
+}
+
+// insertPreferences adds a new preferences to the database for the user.
+func (u *UserPreferencesApp) insertPreferences(username, prefs string) error {
+	query := `INSERT INTO user_preferences (user_id, preferences)
+                 VALUES ($1, $2)`
+	userID, err := queries.UserID(u.db, username)
+	if err != nil {
+		return err
+	}
+	_, err = u.db.Exec(query, userID, prefs)
+	return err
+}
+
+// updatePreferences updates the preferences in the database for the user.
+func (u *UserPreferencesApp) updatePreferences(username, prefs string) error {
+	query := `UPDATE ONLY user_preferences
+                    SET preferences = $2
+                  WHERE user_id = $1`
+	userID, err := queries.UserID(u.db, username)
+	if err != nil {
+		return err
+	}
+	_, err = u.db.Exec(query, userID, prefs)
+	return err
+}
+
+// deletePreferences deletes the user's preferences from the database.
+func (u *UserPreferencesApp) deletePreferences(username string) error {
+	query := `DELETE FROM ONLY user_preferences WHERE user_id = $1`
+	userID, err := queries.UserID(u.db, username)
+	if err != nil {
+		return err
+	}
+	_, err = u.db.Exec(query, userID)
+	return err
+}
+
+// Greeting prints out a greeting to the writer.
+func (u *UserPreferencesApp) Greeting(writer http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(writer, "Hello from user-preferences.")
+}
+
+func badRequest(writer http.ResponseWriter, msg string) {
+	writer.WriteHeader(http.StatusBadRequest)
+	writer.Write([]byte(msg))
+	logcabin.Error.Print(msg)
+}
+
+func errored(writer http.ResponseWriter, msg string) {
+	writer.WriteHeader(http.StatusInternalServerError)
+	writer.Write([]byte(msg))
+	logcabin.Error.Print(msg)
+}
+
+func (u *UserPreferencesApp) getUserPreferencesForRequest(username string, wrap bool) ([]byte, error) {
+	prefs, err := u.getPreferences(username)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting preferences for username %s: %s", username, err)
+	}
+	var retval UserPreferencesRecord
+	if len(prefs) >= 1 {
+		retval = prefs[0]
+	}
+	response, err := convert(&retval, wrap)
+	if err != nil {
+		return nil, fmt.Errorf("Error generating response for username %s: %s", username, err)
+	}
+	var jsoned []byte
+	if len(response) > 0 {
+		jsoned, err = json.Marshal(response)
+		if err != nil {
+			return nil, fmt.Errorf("Error generating session JSON for user %s: %s", username, err)
+		}
+	} else {
+		jsoned = []byte("{}")
+	}
+	return jsoned, nil
+}
+
+// GetRequest handles writing out a user's session as a response.
+func (u *UserPreferencesApp) GetRequest(writer http.ResponseWriter, r *http.Request) {
+	var (
+		username   string
+		userExists bool
+		err        error
+		ok         bool
+		v          = mux.Vars(r)
+	)
+	if username, ok = v["username"]; !ok {
+		badRequest(writer, "Missing username in URL")
+		return
+	}
+	logcabin.Info.Printf("Getting user preferences for %s", username)
+	if userExists, err = queries.IsUser(u.db, username); err != nil {
+		badRequest(writer, fmt.Sprintf("Error checking for username %s: %s", username, err))
+		return
+	}
+	if !userExists {
+		badRequest(writer, fmt.Sprintf("User %s does not exist", username))
+		return
+	}
+	jsoned, err := u.getUserPreferencesForRequest(username, false)
+	if err != nil {
+		errored(writer, err.Error())
+	}
+	writer.Write(jsoned)
+}
+
+// PutRequest handles creating a new user session.
+func (u *UserPreferencesApp) PutRequest(writer http.ResponseWriter, r *http.Request) {
+	u.PostRequest(writer, r)
+}
+
+// PostRequest handles modifying an existing user session.
+func (u *UserPreferencesApp) PostRequest(writer http.ResponseWriter, r *http.Request) {
+	var (
+		username   string
+		userExists bool
+		hasPrefs   bool
+		err        error
+		ok         bool
+		v          = mux.Vars(r)
+	)
+	if username, ok = v["username"]; !ok {
+		badRequest(writer, "Missing username in URL")
+		return
+	}
+	if userExists, err = queries.IsUser(u.db, username); err != nil {
+		badRequest(writer, fmt.Sprintf("Error checking for username %s: %s", username, err))
+		return
+	}
+	if !userExists {
+		badRequest(writer, fmt.Sprintf("User %s does not exist", username))
+		return
+	}
+	if hasPrefs, err = u.hasPreferences(username); err != nil {
+		errored(writer, fmt.Sprintf("Error checking session for user %s: %s", username, err))
+		return
+	}
+	var checked map[string]interface{}
+	bodyBuffer, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errored(writer, fmt.Sprintf("Error reading body: %s", err))
+		return
+	}
+	if err = json.Unmarshal(bodyBuffer, &checked); err != nil {
+		errored(writer, fmt.Sprintf("Error parsing request body: %s", err))
+		return
+	}
+	bodyString := string(bodyBuffer)
+	if !hasPrefs {
+		if err = u.insertPreferences(username, bodyString); err != nil {
+			errored(writer, fmt.Sprintf("Error inserting preferences for user %s: %s", username, err))
+			return
+		}
+	} else {
+		if err = u.updatePreferences(username, bodyString); err != nil {
+			errored(writer, fmt.Sprintf("Error updating preferences for user %s: %s", username, err))
+			return
+		}
+	}
+	jsoned, err := u.getUserPreferencesForRequest(username, true)
+	if err != nil {
+		errored(writer, err.Error())
+		return
+	}
+	writer.Write(jsoned)
+}
+
+// DeleteRequest handles deleting a user session.
+func (u *UserPreferencesApp) DeleteRequest(writer http.ResponseWriter, r *http.Request) {
+	var (
+		username   string
+		userExists bool
+		hasPrefs   bool
+		err        error
+		ok         bool
+		v          = mux.Vars(r)
+	)
+	if username, ok = v["username"]; !ok {
+		badRequest(writer, "Missing username in URL")
+		return
+	}
+	if userExists, err = queries.IsUser(u.db, username); err != nil {
+		badRequest(writer, fmt.Sprintf("Error checking for username %s: %s", username, err))
+		return
+	}
+	if !userExists {
+		badRequest(writer, fmt.Sprintf("User %s does not exist", username))
+		return
+	}
+	if hasPrefs, err = u.hasPreferences(username); err != nil {
+		errored(writer, fmt.Sprintf("Error checking preferences for user %s: %s", username, err))
+		return
+	}
+	if !hasPrefs {
+		return
+	}
+	if err = u.deletePreferences(username); err != nil {
+		errored(writer, fmt.Sprintf("Error deleting preferences for user %s: %s", username, err))
+	}
+}
+
+func newRouter(a App) *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", a.Greeting).Methods("GET")
+	router.HandleFunc("/{username}", a.GetRequest).Methods("GET")
+	router.HandleFunc("/{username}", a.PutRequest).Methods("PUT")
+	router.HandleFunc("/{username}", a.PostRequest).Methods("POST")
+	router.HandleFunc("/{username}", a.DeleteRequest).Methods("DELETE")
+	return router
+}
+
+func fixAddr(addr string) string {
+	if !strings.HasPrefix(addr, ":") {
+		return fmt.Sprintf(":%s", addr)
+	}
+	return addr
+}
+
+func main() {
+	if *version {
+		AppVersion()
+		os.Exit(0)
+	}
+	if *cfgPath == "" {
+		logcabin.Error.Fatal("--config must be set")
+	}
+	if err := configurate.Init(*cfgPath); err != nil {
+		logcabin.Error.Fatal(err)
+	}
+	dburi, err := configurate.C.String("db.uri")
+	if err != nil {
+		logcabin.Error.Fatal(err)
+	}
+	logcabin.Info.Println("Connecting to the database...")
+	db, err := sql.Open("postgres", dburi)
+	if err != nil {
+		logcabin.Error.Fatal(err)
+	}
+	defer db.Close()
+	logcabin.Info.Println("Connected to the database.")
+	if err := db.Ping(); err != nil {
+		logcabin.Error.Fatal(err)
+	}
+	logcabin.Info.Println("Successfully pinged the database")
+	logcabin.Info.Printf("Listening on port %s", *port)
+	app := New(db)
+	logcabin.Error.Fatal(http.ListenAndServe(fixAddr(*port), newRouter(app)))
+}
