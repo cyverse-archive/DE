628841859a87b800d6042074b197d85e23b40c30
Dennis Roberts <dennis@iplantcollaborative.org>
2016-04-26 09:50:31 -0700

    modified the permissions database init scripts so that they're compatible with Postgres 9.3

--- databases/permissions/src/main/constraints/resource_types.sql
+++ databases/permissions/src/main/constraints/resource_types.sql
@@ -1,5 +1,5 @@
 --
 -- Uniqueness constraint on the resource type name.
 --
-CREATE UNIQUE INDEX IF NOT EXISTS resource_types_name_unique
+CREATE UNIQUE INDEX resource_types_name_unique
     ON resource_types (lower(trim(regexp_replace(name, '\s+', ' ', 'g'))));
