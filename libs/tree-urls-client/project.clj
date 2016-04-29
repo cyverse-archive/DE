62a4952f5e5fd563663435554077cb3bc3d457a7
Ian McEwen <mian@iplantcollaborative.org>
2016-04-29 14:00:35 -0700

    Split tree urls client out from terrain and into its own library, in anticipation of using it in data-info

---
+++ libs/tree-urls-client/project.clj
@@ -0,0 +1,9 @@
+(defproject org.iplantc/tree-urls-client "5.2.7.0"
+  :description "Client for the tree-urls service"
+  :url "https://github.com/cyverse/DE"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.iplantc/clojure-commons "5.2.7.0" :exclusions [buddy/buddy-sign metosin/compojure-api metosin/ring-http-response ring]]
+                 [slingshot "0.12.2"]
+                 [clj-http "2.0.0"]
+                 [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
+                 [cheshire "5.5.0"]])
