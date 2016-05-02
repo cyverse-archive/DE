006e0468aab958b5609e80c72da20c25c77b9b2f
Ashley Ramsey <aramsey@cyverse.org>
2016-05-02 15:06:06 -0700

    CORE-7627 Remove unnecessary ontology services

--- ui/de-lib/src/main/java/org/iplantc/de/admin/desktop/client/ontologies/service/OntologyServiceFacade.java
+++ ui/de-lib/src/main/java/org/iplantc/de/admin/desktop/client/ontologies/service/OntologyServiceFacade.java
@@ -3,7 +3,6 @@ package org.iplantc.de.admin.desktop.client.ontologies.service;
 import org.iplantc.de.client.models.apps.App;
 import org.iplantc.de.client.models.ontologies.Ontology;
 import org.iplantc.de.client.models.ontologies.OntologyHierarchy;
-import org.iplantc.de.client.models.ontologies.OntologyHierarchyFilterReq;
 import org.iplantc.de.client.models.ontologies.OntologyMetadata;
 import org.iplantc.de.client.models.ontologies.OntologyVersionDetail;
 
@@ -31,41 +30,12 @@ public interface OntologyServiceFacade {
      */
     void getOntologyHierarchies(String version, AsyncCallback<List<OntologyHierarchy>> callback);
 
-
     /**
-     * Get the list of hierarchies and their subclasses for the active version of an ontology
-     * @param callback
-     */
-    void getActiveOntologyHierarchies(AsyncCallback<List<OntologyHierarchy>> callback);
-
-    /**
-     * Get the list of ontology hierarchies for a specific version of an ontology at the specified root IRI
-     * with a set of target IDs.
+     * Get the list of apps that are not tagged with the given root for the specified Ontology version
      * @param version
      * @param root
-     * @param filter
      * @param callback
      */
-    void filterOntologyHierarchies(String version, String root, OntologyHierarchyFilterReq filter, AsyncCallback<List<OntologyHierarchy>> callback);
-
-    /**
-     * Get the list of ontology hierarchies for the active ontology version at the specified root IRI
-     * @param root
-     * @param callback
-     */
-    void filterActiveOntologyHierarchies(String root, AsyncCallback<List<OntologyHierarchy>> callback);
-
-    /**
-     * Filter a list of target-ids from a specific ontology version at the specified root down to
-     * the orphaned target-ids
-     * @param version
-     * @param root
-     * @param filter
-     * @param callback
-     */
-    void filterUnclassifiedTargets(String version, String root, OntologyHierarchyFilterReq filter, AsyncCallback<List<String>> callback);
-
-
     void getUnclassifiedApps(String version, String root, AsyncCallback<List<App>> callback);
     /**
      * Set the active ontology version used by the app-hierarchies endpoints
