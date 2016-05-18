4716b666f66603040be113f14ea6d55b1c35e3a0
Dennis Roberts <dennisroberts71@gmail.com>
2016-05-18 16:44:10 -0700

    CORE-7662: added several integration tests for the endpoints to add and list permissions

--- golang/src/permissions/restapi/impl/test/permissions_test.go
+++ golang/src/permissions/restapi/impl/test/permissions_test.go
@@ -71,6 +71,9 @@ func TestGrantPermission(t *testing.T) {
 	permission := grantPermission(db, subjectIn, resourceIn, "own")
 
 	// Verify that we got the expected result.
+	if len(permission.ID) != 36 {
+		t.Errorf("unexpected internal permission ID returned: %s", permission.ID)
+	}
 	if permission.Subject.ID != subjectOut.ID {
 		t.Errorf("unexpected internal subject ID returned: %s", permission.Subject.ID)
 	}
@@ -91,5 +94,222 @@ func TestGrantPermission(t *testing.T) {
 	}
 	if permission.PermissionLevel != models.PermissionLevel("own") {
 		t.Errorf("unexpected permission level returned: %v", permission.PermissionLevel)
+	}
+}
+
+func TestListPermissions(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+
+	// Initialize the database.
+	db := initdb(t)
+	addDefaultResourceTypes(db, t)
+
+	// Define a subject.
+	subjectIn := newSubjectIn("s1", "user")
+	subjectOut := addSubject(db, subjectIn.SubjectID, subjectIn.SubjectType)
+
+	// Define a resource.
+	resourceIn := newResourceIn("r1", "app")
+	resourceOut := addResource(db, *resourceIn.Name, *resourceIn.ResourceType)
+
+	// Grant the subject access to the resource.
+	_ = grantPermission(db, subjectIn, resourceIn, "own")
+
+	// List the permissions and verify that we get the expected number of results.
+	permissions := listPermissions(db).Permissions
+	if len(permissions) != 1 {
+		t.Fatalf("unexpected number of permissions listed: %d", len(permissions))
+	}
+
+	// Verify that we got the expected result.
+	permission := permissions[0]
+	if len(permission.ID) != 36 {
+		t.Errorf("unexpected internal permission ID returned: %s", permission.ID)
+	}
+	if permission.Subject.ID != subjectOut.ID {
+		t.Errorf("unexpected internal subject ID listed: %s", permission.Subject.ID)
+	}
+	if permission.Subject.SubjectID != subjectOut.SubjectID {
+		t.Errorf("unexpected external subject ID listed: %s", permission.Subject.SubjectID)
+	}
+	if permission.Subject.SubjectType != subjectOut.SubjectType {
+		t.Errorf("unexpedted subject type listed: %s", permission.Subject.SubjectType)
+	}
+	if *permission.Resource.ID != *resourceOut.ID {
+		t.Errorf("unexpected resource ID listed: %s", *permission.Resource.ID)
+	}
+	if *permission.Resource.Name != *resourceOut.Name {
+		t.Errorf("unexpected resource name listed: %s", *permission.Resource.Name)
+	}
+	if *permission.Resource.ResourceType != *resourceOut.ResourceType {
+		t.Errorf("unexpected resource type listed: %s", *permission.Resource.ResourceType)
+	}
+	if permission.PermissionLevel != models.PermissionLevel("own") {
+		t.Errorf("unexpected permission level listed: %v", permission.PermissionLevel)
+	}
+}
+
+func TestAutoInsertSubject(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+
+	// Initialize the database.
+	db := initdb(t)
+	addDefaultResourceTypes(db, t)
+
+	// Create, but don't register, a subject.
+	subjectIn := newSubjectIn("s1", "user")
+
+	// Define a resource.
+	resourceIn := newResourceIn("r1", "app")
+	resourceOut := addResource(db, *resourceIn.Name, *resourceIn.ResourceType)
+
+	// Grant the subject access to the resource.
+	_ = grantPermission(db, subjectIn, resourceIn, "own")
+
+	// List the permissions and verify that we get the expected number of results.
+	permissions := listPermissions(db).Permissions
+	if len(permissions) != 1 {
+		t.Fatalf("unexpected number of permissions listed: %d", len(permissions))
+	}
+
+	// Verify that we got the expected result.
+	permission := permissions[0]
+	if len(permission.ID) != 36 {
+		t.Errorf("unexpected internal permission ID returned: %s", permission.ID)
+	}
+	if len(permission.Subject.ID) != 36 {
+		t.Error("unexpected internal subject ID listed: %s", permission.Subject.ID)
+	}
+	if permission.Subject.SubjectID != subjectIn.SubjectID {
+		t.Errorf("unexpected external subject ID listed: %s", permission.Subject.SubjectID)
+	}
+	if permission.Subject.SubjectType != subjectIn.SubjectType {
+		t.Errorf("unexpedted subject type listed: %s", permission.Subject.SubjectType)
+	}
+	if *permission.Resource.ID != *resourceOut.ID {
+		t.Errorf("unexpected resource ID listed: %s", *permission.Resource.ID)
+	}
+	if *permission.Resource.Name != *resourceOut.Name {
+		t.Errorf("unexpected resource name listed: %s", *permission.Resource.Name)
+	}
+	if *permission.Resource.ResourceType != *resourceOut.ResourceType {
+		t.Errorf("unexpected resource type listed: %s", *permission.Resource.ResourceType)
+	}
+	if permission.PermissionLevel != models.PermissionLevel("own") {
+		t.Errorf("unexpected permission level listed: %v", permission.PermissionLevel)
+	}
+}
+
+func TestAutoInsertResource(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+
+	// Initialize the database.
+	db := initdb(t)
+	addDefaultResourceTypes(db, t)
+
+	// Define a subject.
+	subjectIn := newSubjectIn("s1", "user")
+	subjectOut := addSubject(db, subjectIn.SubjectID, subjectIn.SubjectType)
+
+	// Create, but don't register, a subject.
+	resourceIn := newResourceIn("r1", "app")
+
+	// Grant the subject access to the resource.
+	_ = grantPermission(db, subjectIn, resourceIn, "own")
+
+	// List the permissions and verify that we get the expected number of results.
+	permissions := listPermissions(db).Permissions
+	if len(permissions) != 1 {
+		t.Fatalf("unexpected number of permissions listed: %d", len(permissions))
+	}
+
+	// Verify that we got the expected result.
+	permission := permissions[0]
+	if len(permission.ID) != 36 {
+		t.Errorf("unexpected internal permission ID returned: %s", permission.ID)
+	}
+	if permission.Subject.ID != subjectOut.ID {
+		t.Errorf("unexpected internal subject ID listed: %s", permission.Subject.ID)
+	}
+	if permission.Subject.SubjectID != subjectOut.SubjectID {
+		t.Errorf("unexpected external subject ID listed: %s", permission.Subject.SubjectID)
+	}
+	if permission.Subject.SubjectType != subjectOut.SubjectType {
+		t.Errorf("unexpedted subject type listed: %s", permission.Subject.SubjectType)
+	}
+	if permission.Resource.ID == nil {
+		t.Error("no resource ID listed")
+	}
+	if *permission.Resource.Name != *resourceIn.Name {
+		t.Errorf("unexpected resource name listed: %s", *permission.Resource.Name)
+	}
+	if *permission.Resource.ResourceType != *resourceIn.ResourceType {
+		t.Errorf("unexpected resource type listed: %s", *permission.Resource.ResourceType)
+	}
+	if permission.PermissionLevel != models.PermissionLevel("own") {
+		t.Errorf("unexpected permission level listed: %v", permission.PermissionLevel)
+	}
+}
+
+func TestUpdatePermissionLevel(t *testing.T) {
+	if !shouldrun() {
+		return
+	}
+
+	// Initialize the database.
+	db := initdb(t)
+	addDefaultResourceTypes(db, t)
+
+	// Define a subject.
+	subjectIn := newSubjectIn("s1", "user")
+	subjectOut := addSubject(db, subjectIn.SubjectID, subjectIn.SubjectType)
+
+	// Define a resource.
+	resourceIn := newResourceIn("r1", "app")
+	resourceOut := addResource(db, *resourceIn.Name, *resourceIn.ResourceType)
+
+	// Grant the subject access to the resource.
+	_ = grantPermission(db, subjectIn, resourceIn, "own")
+
+	// Revise the ownership level.
+	_ = grantPermission(db, subjectIn, resourceIn, "write")
+
+	// List the permissions and verify that we get the expected number of results.
+	permissions := listPermissions(db).Permissions
+	if len(permissions) != 1 {
+		t.Fatalf("unexpected number of permissions listed: %d", len(permissions))
+	}
+
+	// Verify that we got the expected result.
+	permission := permissions[0]
+	if len(permission.ID) != 36 {
+		t.Errorf("unexpected internal permission ID returned: %s", permission.ID)
+	}
+	if permission.Subject.ID != subjectOut.ID {
+		t.Errorf("unexpected internal subject ID listed: %s", permission.Subject.ID)
+	}
+	if permission.Subject.SubjectID != subjectOut.SubjectID {
+		t.Errorf("unexpected external subject ID listed: %s", permission.Subject.SubjectID)
+	}
+	if permission.Subject.SubjectType != subjectOut.SubjectType {
+		t.Errorf("unexpedted subject type listed: %s", permission.Subject.SubjectType)
+	}
+	if *permission.Resource.ID != *resourceOut.ID {
+		t.Errorf("unexpected resource ID listed: %s", *permission.Resource.ID)
+	}
+	if *permission.Resource.Name != *resourceOut.Name {
+		t.Errorf("unexpected resource name listed: %s", *permission.Resource.Name)
+	}
+	if *permission.Resource.ResourceType != *resourceOut.ResourceType {
+		t.Errorf("unexpected resource type listed: %s", *permission.Resource.ResourceType)
+	}
+	if permission.PermissionLevel != models.PermissionLevel("write") {
+		t.Errorf("unexpected permission level listed: %v", permission.PermissionLevel)
 	}
 }
