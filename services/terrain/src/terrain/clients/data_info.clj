cyverse-archive/DE

commit 8bb831aad8ff75e634217467f88fc55ace30fae5

CORE-6259: Make clients.data-info/stat-by-uuid use data-info

services/terrain/src/terrain/clients/data_info.clj
@@ -3,6 +3,7 @@
         [slingshot.slingshot :only [throw+ try+]])
   (:require [clojure.string :as string]
             [clojure.tools.logging :as log]
+            [clojure.walk :as walk]
             [cemerick.url :as url]
             [cheshire.core :as json]
             [me.raynes.fs :as fs]
@@ -286,8 +287,11 @@
    Returns:
      It returns a path-stat map containing an additional UUID field."
   [^String user ^UUID uuid]
-  (uuids/path-for-uuid user uuid))
-
+  (-> (raw/collect-stats user :ids [uuid])
+      :body
+      json/decode
+      (get-in ["ids" uuid])
+      walk/keywordize-keys))
 
 (defn ^ISeq stats-by-uuids-paged
   "Resolves the stat info for the entities with the given UUIDs. The results are paged.

commit 2265b581d0565ba24e1f0e336ae456921b396df2

Fix bug in terrain.clients.data-info/stat-by-uuid
Introduced by commit 8bb831aad8ff75e634217467f88fc55ace30fae5 (CORE-6259).

services/terrain/src/terrain/clients/data_info.clj
@@ -296,7 +296,7 @@
   (-> (raw/collect-stats user :ids [uuid])
       :body
       json/decode
-      (get-in ["ids" uuid])
+      (get-in ["ids" (str uuid)])
       walk/keywordize-keys))
 
 (defn ^ISeq stats-by-uuids-paged