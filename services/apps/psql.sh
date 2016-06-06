9ec47312d964d932fb1e1c3da315c08618bffafd
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-06-06 13:02:17 -0700

    Modified apps' psql.sh to hit the apps-de-db instance

--- services/apps/psql.sh
+++ services/apps/psql.sh
@@ -4,4 +4,4 @@ echo "This script and docker container are intended for use against the database
 echo ''
 echo "The password for the 'de' user to the 'de' database is 'notprod' (without the quotes)."
 
-docker run -it --link de-db:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U de -d de'
+docker run -it --link apps-de-db:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U de -d de'
