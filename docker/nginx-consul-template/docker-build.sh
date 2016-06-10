5b04ce3934cd432ccb382973ed0f5070fe95f412
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-06-10 11:23:04 -0700

    DOCKER_USER, DOCKER_REPO, DOCKER_TAG have defaults for docker/nginx-consul-template/docker-build.sh

--- docker/nginx-consul-template/docker-build.sh
+++ docker/nginx-consul-template/docker-build.sh
@@ -2,7 +2,19 @@
 set -x
 set -e
 
+if [ -z "$DOCKER_USER" ]; then
+	DOCKER_USER=discoenv
+fi
+
+if [ -z "$DOCKER_REPO" ]; then
+	DOCKER_REPO=nginx-consul-template
+fi
+
+if [ -z "$DOCKER_TAG" ]; then
+	DOCKER_TAG=dev
+fi
+
 GIT_COMMIT=$(git rev-parse HEAD)
 
-docker build --build-arg git_commit=$GIT_COMMIT --pull --no-cache --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
-docker push $DOCKER_USER/$DOCKER_REPO:dev
+docker build --build-arg git_commit=$GIT_COMMIT --pull --no-cache --rm -t "$DOCKER_USER/$DOCKER_REPO:$DOCKER_TAG" .
+docker push $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAG
