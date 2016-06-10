fc42cccc1df9c63a4e517e658bf003c4a58a58ce
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-06-10 11:10:33 -0700

    DOCKER_USER, DOCKER_REPO, DOCKER_TAG have defaults in kifshare's build script

--- services/kifshare/docker-build.sh
+++ services/kifshare/docker-build.sh
@@ -2,6 +2,18 @@
 set -x
 set -e
 
+if [ -z "$DOCKER_USER" ]; then
+	DOCKER_USER=discoenv
+fi
+
+if [ -z "$DOCKER_REPO" ]; then
+	DOCKER_REPO=kifshare
+fi
+
+if [ -z "$DOCKER_TAG" ]; then
+	DOCKER_TAG=dev
+fi
+
 VERSION=$(cat version | sed -e 's/^ *//' -e 's/ *$//')
 GIT_COMMIT=$(git rev-parse HEAD)
 
@@ -14,7 +26,7 @@ docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$GIT_COMMIT" -v $(pwd):/bu
 docker build --build-arg git_commit=$GIT_COMMIT \
              --build-arg buildenv_git_commit=$BUILDENV_GIT_COMMIT \
              --build-arg version=$VERSION \
-             --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:dev" .
+             --pull --rm -t "$DOCKER_USER/$DOCKER_REPO:$DOCKER_TAG" .
 
-docker push $DOCKER_USER/$DOCKER_REPO:dev
-docker rmi $DOCKER_USER/$DOCKER_REPO:dev
+docker push $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAG
+docker rmi $DOCKER_USER/$DOCKER_REPO:$DOCKER_TAG
