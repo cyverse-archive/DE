30a328043b12cf6e6b009ecfa8ce5c7b05c20b25
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-06-10 11:33:07 -0700

    DOCKER_USER, DOCKER_TAG have defaults in template-mover's docker-build.sh

--- tools/template-mover/docker-build.sh
+++ tools/template-mover/docker-build.sh
@@ -2,7 +2,15 @@
 set -x
 set -e
 
+if [ -z "$DOCKER_USER" ]; then
+	DOCKER_USER=discoenv
+fi
+
+if [ -z "$DOCKER_TAG" ]; then
+	DOCKER_TAG=dev
+fi
+
 docker pull discoenv/buildenv:latest
 docker run --rm -t -a stdout -a stderr -e "GIT_COMMIT=$(git rev-parse HEAD)" -v $(pwd):/build -w /build discoenv/buildenv lein uberjar
-docker build --rm -t $DOCKER_USER/template-mover:dev .
-docker push $DOCKER_USER/template-mover:dev
+docker build --rm -t $DOCKER_USER/template-mover:$DOCKER_TAG .
+docker push $DOCKER_USER/template-mover:$DOCKER_TAG
