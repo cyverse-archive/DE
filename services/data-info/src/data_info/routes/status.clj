5d236cc088faaa4f3e1edb68ce3cc955355e1023
Ian McEwen <mian@iplantcollaborative.org>
2016-06-14 16:02:52 -0700

    Add an expecting parameter to data-info

--- services/data-info/src/data_info/routes/status.clj
+++ services/data-info/src/data_info/routes/status.clj
@@ -1,21 +1,25 @@
 (ns data-info.routes.status
   (:use [common-swagger-api.schema]
         [data-info.routes.schemas.status])
-  (:require [clojure-commons.service :as commons-svc]
+  (:require [clojure-commons.service :as commons-service]
+            [clojure-commons.error-codes :as ce]
             [data-info.services.status :as status]
             [data-info.util.config :as config]
             [data-info.util.service :as service]
+            [ring.util.http-response :refer [ok internal-server-error]]
             [schema.core :as s]))
 
 (defroutes* status
     (GET* "/" [:as {:keys [uri server-name server-port]}]
+      :query [{:keys [expecting]} StatusParams]
       :return DataInfoStatusResponse
       :tags ["service-info"]
       :summary "Service Information"
       :description "This endpoint provides the name of the service and its version."
-      (service/trap uri
-        #(assoc (commons-svc/get-docs-status config/svc-info server-name server-port config/docs-uri)
-                :iRODS (status/irods-running?))))
+      (ce/trap uri
+        #((if (and expecting (not= expecting (:app-name config/svc-info))) internal-server-error ok)
+           (assoc (commons-service/get-docs-status config/svc-info server-name server-port config/docs-uri expecting)
+                  :iRODS (status/irods-running?)))))
 
     (GET* "/admin/config" [:as {:keys [uri]}]
       :return (describe s/Any "A map of configuration keys to values.")
