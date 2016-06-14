a2db52b7766365aa01a013ab614301e7b7a0af8e
Ian McEwen <mian@iplantcollaborative.org>
2016-06-14 15:49:30 -0700

    Add an expecting parameter to metadata

--- services/metadata/src/metadata/routes/status.clj
+++ services/metadata/src/metadata/routes/status.clj
@@ -1,16 +1,14 @@
 (ns metadata.routes.status
   (:use [common-swagger-api.schema]
-        [ring.util.http-response :only [ok]])
-  (:require [clojure-commons.service :as commons-svc]
+        [ring.util.http-response :only [ok internal-server-error]])
+  (:require [clojure-commons.service :as commons-service]
             [metadata.util.config :as config]))
 
 (defroutes* status
-  (context* "/" []
-    :tags ["service-info"]
-
-    (GET* "/" [:as {:keys [server-name server-port]}]
-      :return StatusResponse
-      :summary "Service Information"
-      :description "This endpoint provides the name of the service and its version."
-      (ok
-        (commons-svc/get-docs-status config/svc-info server-name server-port config/docs-uri)))))
+  (GET* "/" [:as {:keys [server-name server-port]}]
+    :query [{:keys [expecting]} StatusParams]
+    :return StatusResponse
+    :summary "Service Information"
+    :description "This endpoint provides the name of the service and its version."
+    ((if (and expecting (not= expecting (:app-name config/svc-info))) internal-server-error ok)
+      (commons-service/get-docs-status config/svc-info server-name server-port config/docs-uri expecting))))
