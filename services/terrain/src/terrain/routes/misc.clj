f2264a34e378d6110492ed9a14d07a10b08d2d7f
Ian McEwen <mian@iplantcollaborative.org>
2016-06-15 13:19:36 -0700

    Add an expecting parameter to terrain

--- services/terrain/src/terrain/routes/misc.clj
+++ services/terrain/src/terrain/routes/misc.clj
@@ -1,13 +1,16 @@
 (ns terrain.routes.misc
-  (:use [compojure.core])
+  (:use [compojure.core]
+        [ring.util.http-response])
   (:require [clojure.string :as string])
   (:import [java.util UUID]))
 
 (defn unsecured-misc-routes
   []
   (routes
-    (GET "/" [request]
-      "The infinite is attainable with Terrain!\n")
+    (GET "/" [:as {{expecting :expecting} :params :as req}]
+      (if (and expecting (not= expecting "terrain"))
+        (internal-server-error (str "The infinite is attainable with Terrain!\nError: expecting " expecting "."))
+        (ok "The infinite is attainable with Terrain!\n")))
 
     (GET "/uuid" []
       (string/upper-case (str (UUID/randomUUID))))))
