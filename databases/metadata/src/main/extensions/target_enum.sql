c3411008eca2425b37438033919e6fd57e989047
Paul Sarando <psarando@iplantcollaborative.org>
2016-06-14 18:15:40 -0700

    CORE-7712 Add 'avu' enum to metadata database target_enum type.
    Adds metadata db conversion 2.7.0:20160614.01

--- databases/metadata/src/main/extensions/target_enum.sql
+++ databases/metadata/src/main/extensions/target_enum.sql
@@ -1,6 +1,5 @@
 --
 -- target type enumeration
--- Currently, only analyses, apps, files, folders, and users may be targets of metadata.
+-- Currently, only analyses, apps, other AVUs, files, folders, and users may be targets of metadata.
 --
-CREATE TYPE target_enum AS ENUM ('analysis', 'app', 'file', 'folder', 'user');
-
+CREATE TYPE target_enum AS ENUM ('analysis', 'app', 'avu', 'file', 'folder', 'avu', 'user');
