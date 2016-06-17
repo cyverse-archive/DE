0137eaf503fdd00ffe9df5f9fb00a0615891ef07
Dennis Roberts <dennis@iplantcollaborative.org>
2016-06-16 17:30:22 -0700

    CORE-7691: added tests corresponding to core-7008.py and part of core-7009.py

--- services/apps/test/apps/service/apps/permissions_test.clj
+++ services/apps/test/apps/service/apps/permissions_test.clj
@@ -11,7 +11,8 @@
             [apps.util.config :as config]
             [clojure.tools.logging :as log]
             [korma.core :as sql]
-            [permissions-client.core :as pc]))
+            [permissions-client.core :as pc])
+  (:import [clojure.lang ExceptionInfo]))
 
 (defn create-user [i]
   (let [username (str "testde" i)]
@@ -116,8 +117,17 @@
 (defn get-category [user category-name]
   (find-category category-name (:categories (apps/get-app-categories user {}))))
 
+(defn get-dev-category [user]
+  (get-category user "Apps under development"))
+
+(defn get-beta-category [user]
+  (get-category user "Beta"))
+
 (defn get-admin-category [user category-name]
   (find-category category-name (:categories (apps/get-admin-app-categories user {}))))
+
+(defn get-admin-beta-category [user]
+  (get-admin-category user "Beta"))
 
 (deftest test-app-search
   (let [{username :shortUsername :as user} (get-user :testde1)]
@@ -132,8 +142,8 @@
 
 (deftest test-app-category-listing-counts
   (let [{username :shortUsername :as user} (get-user :testde1)
-        dev-category-id                    (:id (get-category user "Apps under development"))
-        beta-category-id                   (:id (get-category user "Beta"))
+        dev-category-id                    (:id (get-dev-category user))
+        beta-category-id                   (:id (get-beta-category user))
         group-id                           (ipg/grouper-user-group-id)]
     (is (= 1 (:app_count (apps/list-apps-in-category user dev-category-id {}))))
     (is (= (count beta-apps) (:app_count (apps/list-apps-in-category user beta-category-id {}))))
@@ -149,33 +159,77 @@
 (deftest test-app-hierarchy-counts
   (let [{username :shortUsername :as user} (get-user :testde1)
         group-id                           (ipg/grouper-user-group-id)]
-    (is (= 1 (:app_count (get-category user "Apps under development"))))
-    (is (= (count beta-apps) (:app_count (get-category user "Beta"))))
+    (is (= 1 (:app_count (get-dev-category user))))
+    (is (= (count beta-apps) (:app_count (get-beta-category user))))
     (perms-client/unshare-app (:id test-app) "user" username)
-    (is (= 0 (:app_count (get-category user "Apps under development"))))
-    (is (= (count beta-apps) (:app_count (get-category user "Beta"))))
+    (is (= 0 (:app_count (get-dev-category user))))
+    (is (= (count beta-apps) (:app_count (get-beta-category user))))
     (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" username "own")
-    (is (= 1 (:app_count (get-category user "Apps under development"))))
-    (is (= (count beta-apps) (:app_count (get-category user "Beta"))))
+    (is (= 1 (:app_count (get-dev-category user))))
+    (is (= (count beta-apps) (:app_count (get-beta-category user))))
     (pc/revoke-permission (config/permissions-client) "app" (:id (first beta-apps)) "group" group-id)
-    (is (= 1 (:app_count (get-category user "Apps under development"))))
-    (is (= (dec (count beta-apps)) (:app_count (get-category user "Beta"))))))
+    (is (= 1 (:app_count (get-dev-category user))))
+    (is (= (dec (count beta-apps)) (:app_count (get-beta-category user))))))
 
 (deftest test-admin-app-hierarchy-counts
   (let [{username :shortUsername :as user} (get-user :testde1)
         group-id                           (ipg/grouper-user-group-id)]
-    (is (= (count beta-apps) (:app_count (get-admin-category user "Beta"))))
+    (is (= (count beta-apps) (:app_count (get-admin-beta-category user))))
     (pc/revoke-permission (config/permissions-client) "app" (:id (first beta-apps)) "group" group-id)
-    (is (= (dec (count beta-apps)) (:app_count (get-admin-category user "Beta"))))))
+    (is (= (dec (count beta-apps)) (:app_count (get-admin-beta-category user))))))
 
 (defn find-app [listing app-id]
   (first (filter (comp (partial = app-id) :id) (:apps listing))))
 
 (deftest test-app-category-listing
   (let [{username :shortUsername :as user} (get-user :testde1)
-        beta-category-id                   (:id (get-category user "Beta"))
+        beta-category-id                   (:id (get-beta-category user))
         group-id                           (ipg/grouper-user-group-id)
         app-id                             (:id (first beta-apps))]
     (is (find-app (apps/list-apps-in-category user beta-category-id {}) app-id))
     (pc/revoke-permission (config/permissions-client) "app" (:id (first beta-apps)) "group" group-id)
     (is (nil? (find-app (apps/list-apps-in-category user beta-category-id {}) app-id)))))
+
+(deftest check-initial-ownership-permission
+  (let [{username :shortUsername :as user} (get-user :testde1)
+        dev-category-id                    (:id (get-dev-category user))]
+    (is (= "own" (:permission (first (:apps (apps/list-apps-in-category user dev-category-id {}))))))))
+
+(defn check-delete-apps [user]
+  (apps/delete-apps user {:app_ids [(:id test-app)]}))
+
+(defn check-delete-app [user]
+  (apps/delete-app user (:id test-app)))
+
+(defn check-relabel-app [user]
+  (apps/relabel-app user test-app))
+
+(defn check-update-app [user]
+  (apps/update-app user test-app))
+
+(defn check-app-ui [user]
+  (apps/get-app-ui user (:id test-app)))
+
+(defn check-copy-app [user]
+  (apps/copy-app user (:id test-app)))
+
+(defn check-edit-app-docs [user]
+  (apps/owner-edit-app-docs user (:id test-app) {:documentation ""}))
+
+(defn check-get-app-details [user]
+  (apps/get-app-details user (:id test-app)))
+
+(defn check-get-app-docs [user]
+  (apps/get-app-docs user (:id test-app)))
+
+(deftest test-permission-restrictions-none
+  (let [user (get-user :testde2)]
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-apps user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-relabel-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-update-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-app-ui user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-copy-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-edit-app-docs user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-get-app-details user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-get-app-docs user)))))
