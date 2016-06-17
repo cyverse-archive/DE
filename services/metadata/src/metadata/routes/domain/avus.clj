33c982571f1ca791c6b1cf99c500c782d1b62007
Paul Sarando <psarando@iplantcollaborative.org>
2016-06-16 17:55:36 -0700

    CORE-7715 Allow metadata AVU's to attach to other metadata AVU’s.
    Updated the metadata service GET/PUT/POST avus endpoints to support each
    AVU having its own `avus` list.

--- services/metadata/src/metadata/routes/domain/avus.clj
+++ services/metadata/src/metadata/routes/domain/avus.clj
@@ -24,7 +24,8 @@
    :created_by (describe String "The ID of the user who created the AVU")
    :modified_by (describe String "The ID of the user who last modified the AVU")
    :created_on (describe Long "The date the AVU was created in ms since the POSIX epoch")
-   :modified_on (describe Long "The date the AVU was last modified in ms since the POSIX epoch")})
+   :modified_on (describe Long "The date the AVU was last modified in ms since the POSIX epoch")
+   (s/optional-key :avus) (describe [(s/recursive #'Avu)] "AVUs attached to this AVU")})
 
 (s/defschema AvuList
   {:avus (describe [Avu] "The list of AVUs associated with the target")})
@@ -36,7 +37,9 @@
       (->optional-param :created_by)
       (->optional-param :modified_by)
       (->optional-param :created_on)
-      (->optional-param :modified_on)))
+      (->optional-param :modified_on)
+      (assoc (s/optional-key :avus)
+             (describe [(s/recursive #'AvuRequest)] "AVUs attached to this AVU"))))
 
 (s/defschema AvuListRequest
   {:avus
--- services/metadata/test/integration_tests/metadata/services/avus_test.clj
+++ services/metadata/test/integration_tests/metadata/services/avus_test.clj
@@ -2,7 +2,8 @@
   (:use [clojure.test]
         [integration-tests]
         [kameleon.uuids :only [uuid]]
-        [metadata.services.avus]))
+        [metadata.services.avus])
+  (:require [metadata.persistence.avu :as persistence]))
 
 (def test-user-id "test_user")
 (def test-target-type "app")
@@ -23,13 +24,15 @@
 
 ;; Test helper functions
 (defn- list-avus->set
-  []
-  (let [avu-list (list-avus test-target-type test-target-id)]
-    (set (map #(select-keys % [:id :attr :value :unit]) (:avus avu-list)))))
+  ([]
+   (list-avus->set test-target-type test-target-id))
+  ([target-type target-id]
+   (let [avu-list (:avus (list-avus target-type target-id))]
+     (set (map #(select-keys % [:id :attr :value :unit]) avu-list)))))
 
-(defn- is-avu-set?
-  [expected-avu-set]
-  (is (= (list-avus->set) expected-avu-set)))
+(defn- deleted-avus?
+  [avus]
+  (empty? (persistence/get-avus-by-ids (map :id avus))))
 
 
 ;; Test AVUs setup, for the following AVU tests
@@ -42,13 +45,14 @@
 
 (deftest simple-set-get-avus-test
   (testing "Set AVUs"
-    (is-avu-set? (set (:avus test-metadata-avus)))
+    (is (= (list-avus->set) (set (:avus test-metadata-avus))))
 
     (set-avus test-user-id test-target-type test-target-id {:avus []})
-    (is-avu-set? #{})
+    (is (= (list-avus->set) #{}))
+    (is (deleted-avus? [test-avu-a test-avu-b]))
 
     (set-avus test-user-id test-target-type test-target-id test-metadata-avus)
-    (is-avu-set? (set (:avus test-metadata-avus)))))
+    (is (= (list-avus->set) (set (:avus test-metadata-avus))))))
 
 
 (deftest update-avus-test
@@ -57,11 +61,11 @@
 
       (testing "Add an AVU"
         (update-avus test-user-id test-target-type test-target-id {:avus [test-avu]})
-        (is-avu-set? (set (conj (:avus test-metadata-avus) test-avu))))
+        (is (= (list-avus->set) (set (conj (:avus test-metadata-avus) test-avu)))))
 
       (testing "Update existing AVU without changes (noop)"
         (update-avus test-user-id test-target-type test-target-id {:avus [(dissoc test-avu :id)]})
-        (is-avu-set? (set (conj (:avus test-metadata-avus) test-avu))))
+        (is (= (list-avus->set) (set (conj (:avus test-metadata-avus) test-avu)))))
 
       (testing "Exception from adding duplicate AVU"
         (is (thrown-with-msg? org.postgresql.util.PSQLException
@@ -71,7 +75,7 @@
 
       (testing "Update existing AVU with changes"
         (update-avus test-user-id test-target-type test-target-id {:avus [modified-avu]})
-        (is-avu-set? (set (conj (:avus test-metadata-avus) modified-avu))))))
+        (is (= (list-avus->set) (set (conj (:avus test-metadata-avus) modified-avu)))))))
 
 
 (deftest set-avus-test
@@ -81,8 +85,71 @@
           test-avu {:id (uuid) :attr "x" :value "y" :unit "z"}]
       (testing "- remove test-avu-b, set test-avu-a with test-avu-b's values"
         (set-avus test-user-id test-target-type test-target-id {:avus [test-avu-a]})
-        (is-avu-set? #{test-avu-a}))
+        (is (= (list-avus->set) #{test-avu-a}))
+        (is (deleted-avus? [test-avu-b])))
 
       (testing "- remove existing, add one new"
         (set-avus test-user-id test-target-type test-target-id {:avus [test-avu]})
-        (is-avu-set? #{test-avu})))))
+        (is (= (list-avus->set) #{test-avu}))
+        (is (deleted-avus? [test-avu-a test-avu-b]))))))
+
+
+(deftest avus-attached-to-avus-test
+  (testing "Attach AVUs to AVUs"
+    (let [test-avu-1 {:id (uuid) :attr "x" :value "a" :unit "z"}
+          test-avu-2 {:id (uuid) :attr "x" :value "b" :unit "z"}
+          test-avu-3 {:id (uuid) :attr "x" :value "c" :unit "z"}
+          test-nested-avus (assoc test-avu-a
+                             :avus [(assoc test-avu-1
+                                      :avus [(assoc test-avu-2
+                                               :avus [test-avu-3])])])
+          modified-avu-1 (assoc (select-keys test-avu-2 [:attr :value :unit])
+                           :id (:id test-avu-1))]
+
+      (testing "- attach test-avu-1 to test-avu-a"
+        (update-avus test-user-id test-target-type test-target-id {:avus [(assoc test-avu-a
+                                                                            :avus [test-avu-1])]})
+        (is (= (list-avus->set) (set (:avus test-metadata-avus))))
+        (is (= (list-avus->set "avu" (:id test-avu-a)) #{test-avu-1})))
+
+      (testing "- remove test-avu-a, add test-avu-b"
+        (set-avus test-user-id test-target-type test-target-id {:avus [test-avu-b]})
+        (is (= (list-avus->set) #{test-avu-b}))
+        (is (empty? (persistence/get-avus-for-target "avu" (:id test-avu-a))))
+        (is (deleted-avus? [test-avu-a test-avu-1])))
+
+      (testing "- attach nested test-avu-1, test-avu-2, and test-avu-3 to test-avu-a"
+        (update-avus test-user-id test-target-type test-target-id {:avus [test-nested-avus]})
+        (is (= (list-avus->set) (set (:avus test-metadata-avus))))
+        (is (= (list-avus->set "avu" (:id test-avu-a)) #{test-avu-1}))
+        (is (= (list-avus->set "avu" (:id test-avu-1)) #{test-avu-2}))
+        (is (= (list-avus->set "avu" (:id test-avu-2)) #{test-avu-3})))
+
+      (testing "- remove test-avu-a, test-avu-1, test-avu-2, and test-avu-3 - add test-avu-b"
+        (set-avus test-user-id test-target-type test-target-id {:avus [test-avu-b]})
+        (is (= (list-avus->set) #{test-avu-b}))
+        (is (empty? (persistence/get-avus-for-target "avu" (:id test-avu-a))))
+        (is (empty? (persistence/get-avus-for-target "avu" (:id test-avu-1))))
+        (is (empty? (persistence/get-avus-for-target "avu" (:id test-avu-2))))
+        (is (deleted-avus? [test-avu-a
+                            test-avu-1
+                            test-avu-2
+                            test-avu-3])))
+
+      (testing "- attach test-avu-1, test-avu-2, and test-avu-3 to test-avu-a - not nested"
+        (update-avus test-user-id test-target-type test-target-id {:avus [(assoc test-avu-a
+                                                                            :avus [test-avu-1
+                                                                                   test-avu-2
+                                                                                   test-avu-3])]})
+        (is (= (list-avus->set) (set (:avus test-metadata-avus))))
+        (is (= (list-avus->set "avu" (:id test-avu-a))
+               #{test-avu-1 test-avu-2 test-avu-3})))
+
+      (testing "- remove test-avu-2, set test-avu-1 with test-avu-2's values"
+        (set-avus test-user-id test-target-type test-target-id {:avus [(assoc test-avu-a
+                                                                         :avus [modified-avu-1
+                                                                                test-avu-3])]})
+        (is (= (list-avus->set) #{test-avu-a}))
+        (is (= (list-avus->set "avu" (:id test-avu-a))
+               #{modified-avu-1 test-avu-3}))
+        (is (deleted-avus? [test-avu-2]))))))
