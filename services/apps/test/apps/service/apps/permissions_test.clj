61538ad27fd973af8c02fd460b920e1817824617
Dennis Roberts <dennis@iplantcollaborative.org>
2016-06-17 18:00:42 -0700

    CORE-7691: more integration test conversions

--- services/apps/test/apps/service/apps/permissions_test.clj
+++ services/apps/test/apps/service/apps/permissions_test.clj
@@ -2,8 +2,9 @@
   (:use [apps.user :only [user-from-attributes]]
         [clojure.test]
         [clostache.parser :only [render]]
-        [kameleon.uuids :only [uuidify]])
+        [kameleon.uuids :only [uuidify uuid]])
   (:require [apps.clients.iplant-groups :as ipg]
+            [apps.clients.notifications :as dn]
             [apps.clients.permissions :as perms-client]
             [apps.service.apps :as apps]
             [apps.service.workspace :as workspace]
@@ -62,7 +63,7 @@
                                 :type            "FileOutput"
                                 :valicators      []
                                 :value           "out.txt"}]}]
-   :name  "Test App"
+   :name  "Permissions Test App"
    :tools [{:attribution ""
             :description "Word Count"
             :id          (uuidify "85cf7a33-386b-46fe-87c7-8c9d59972624")
@@ -75,10 +76,19 @@
 (def ^:dynamic public-apps nil)
 (def ^:dynamic beta-apps nil)
 
+(defn create-test-app
+  ([user]
+   (create-test-app user (:name app-definition)))
+  ([user name]
+   (sql/delete :apps (sql/where {:name name}))
+   (let [app (apps/add-app user (assoc app-definition :name name))]
+     (apps/owner-add-app-docs user (:id app) {:documentation "This is a test."})
+     app)))
+
 (defn with-test-app [f]
-  (binding [test-app (apps/add-app (get-user :testde1) app-definition)]
+  (binding [test-app (create-test-app (get-user :testde1))]
     (f)
-    (apps/permanently-delete-apps (get-user :testde1) {:app_ids [(:id test-app)]})))
+    (apps/permanently-delete-apps (get-user :testde1) {:app_ids [(:id test-app)] :root_deletion_request true})))
 
 (defn with-workspaces [f]
   (dorun (map (comp workspace/get-workspace get-user) (keys users)))
@@ -131,14 +141,14 @@
 
 (deftest test-app-search
   (let [{username :shortUsername :as user} (get-user :testde1)]
-    (is (= 1 (:app_count (apps/search-apps user {:search "Test App"}))))
-    (is (= 1 (count (:apps (apps/search-apps user {:search "Test App"})))))
+    (is (= 1 (:app_count (apps/search-apps user {:search (:name test-app)}))))
+    (is (= 1 (count (:apps (apps/search-apps user {:search (:name test-app)})))))
     (perms-client/unshare-app (:id test-app) "user" username)
-    (is (= 0 (:app_count (apps/search-apps user {:search "Test App"}))))
-    (is (= 0 (count (:apps (apps/search-apps user {:search "Test App"})))))
+    (is (= 0 (:app_count (apps/search-apps user {:search (:name test-app)}))))
+    (is (= 0 (count (:apps (apps/search-apps user {:search (:name test-app)})))))
     (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" username "own")
-    (is (= 1 (:app_count (apps/search-apps user {:search "Test App"}))))
-    (is (= 1 (count (:apps (apps/search-apps user {:search "Test App"})))))))
+    (is (= 1 (:app_count (apps/search-apps user {:search (:name test-app)}))))
+    (is (= 1 (count (:apps (apps/search-apps user {:search (:name test-app)})))))))
 
 (deftest test-app-category-listing-counts
   (let [{username :shortUsername :as user} (get-user :testde1)
@@ -195,32 +205,72 @@
         dev-category-id                    (:id (get-dev-category user))]
     (is (= "own" (:permission (first (:apps (apps/list-apps-in-category user dev-category-id {}))))))))
 
-(defn check-delete-apps [user]
-  (apps/delete-apps user {:app_ids [(:id test-app)]}))
+(defn check-delete-apps
+  ([user]
+   (check-delete-apps user test-app))
+  ([user app]
+   (apps/delete-apps user {:app_ids [(:id app)]})
+   true))
 
-(defn check-delete-app [user]
-  (apps/delete-app user (:id test-app)))
+(defn check-delete-app
+  ([user]
+   (check-delete-app user test-app))
+  ([user app]
+   (apps/delete-app user (:id app))
+   true))
 
 (defn check-relabel-app [user]
-  (apps/relabel-app user test-app))
+  (apps/relabel-app user test-app)
+  true)
 
 (defn check-update-app [user]
-  (apps/update-app user test-app))
+  (apps/update-app user test-app)
+  true)
 
 (defn check-app-ui [user]
-  (apps/get-app-ui user (:id test-app)))
+  (apps/get-app-ui user (:id test-app))
+  true)
 
 (defn check-copy-app [user]
-  (apps/copy-app user (:id test-app)))
+  (let [app-id (:id (apps/copy-app user (:id test-app)))]
+    (apps/permanently-delete-apps user {:app_ids [app-id]}))
+  true)
 
 (defn check-edit-app-docs [user]
-  (apps/owner-edit-app-docs user (:id test-app) {:documentation ""}))
+  (apps/owner-edit-app-docs user (:id test-app) {:documentation ""})
+  true)
 
 (defn check-get-app-details [user]
-  (apps/get-app-details user (:id test-app)))
+  (apps/get-app-details user (:id test-app))
+  true)
 
 (defn check-get-app-docs [user]
-  (apps/get-app-docs user (:id test-app)))
+  (apps/get-app-docs user (:id test-app))
+  true)
+
+(defn check-favorite [user]
+  (apps/add-app-favorite user (:id test-app))
+  true)
+
+(defn check-publishable [user]
+  (apps/app-publishable? user (:id test-app))
+  true)
+
+(defn check-rating [user]
+  (apps/rate-app user (:id test-app) {:rating 5 :comment_id 27})
+  true)
+
+(defn check-unrating [user]
+  (apps/delete-app-rating user (:id test-app))
+  true)
+
+(defn check-tasks [user]
+  (apps/get-app-task-listing user (:id test-app))
+  true)
+
+(defn check-tools [user]
+  (apps/get-app-tool-listing user (:id test-app))
+  true)
 
 (deftest test-permission-restrictions-none
   (let [user (get-user :testde2)]
@@ -232,4 +282,162 @@
     (is (thrown-with-msg? ExceptionInfo #"privileges" (check-copy-app user)))
     (is (thrown-with-msg? ExceptionInfo #"privileges" (check-edit-app-docs user)))
     (is (thrown-with-msg? ExceptionInfo #"privileges" (check-get-app-details user)))
-    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-get-app-docs user)))))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-get-app-docs user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-favorite user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-publishable user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-rating user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-unrating user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-tasks user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-tools user)))))
+
+(deftest test-permission-restrictions-read
+  (let [{username :shortUsername :as user} (get-user :testde2)]
+    (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" username "read")
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-apps user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-relabel-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-update-app user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-app-ui user)))
+    (is (check-copy-app user))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-edit-app-docs user)))
+    (is (check-get-app-details user))
+    (is (check-get-app-docs user))
+    (is (check-favorite user))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-publishable user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-rating user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-unrating user)))
+    (is (check-tasks user))
+    (is (check-tools user))))
+
+(deftest test-permission-restrictions-write
+  (let [{username :shortUsername :as user} (get-user :testde2)]
+    (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" username "write")
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-apps user)))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-delete-app user)))
+    (is (check-relabel-app user))
+    (is (check-update-app user))
+    (is (check-app-ui user))
+    (is (check-copy-app user))
+    (is (check-edit-app-docs user))
+    (is (check-get-app-details user))
+    (is (check-get-app-docs user))
+    (is (check-favorite user))
+    (is (thrown-with-msg? ExceptionInfo #"privileges" (check-publishable user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-rating user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-unrating user)))
+    (is (check-tasks user))
+    (is (check-tools user))))
+
+(deftest test-permission-restrictions-own
+  (let [{username :shortUsername :as user} (get-user :testde2)]
+    (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" username "own")
+    (is (check-relabel-app user))
+    (is (check-update-app user))
+    (is (check-app-ui user))
+    (is (check-copy-app user))
+    (is (check-edit-app-docs user))
+    (is (check-get-app-details user))
+    (is (check-get-app-docs user))
+    (is (check-favorite user))
+    (is (check-publishable user))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-rating user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-unrating user)))
+    (is (check-tasks user))
+    (is (check-tools user))
+    (let [app (create-test-app user "Shreddable")]
+      (is (check-delete-apps user app))
+      (apps/permanently-delete-apps user {:app_ids [(:id app)]}))
+    (let [app (create-test-app user "Deletable")]
+      (is (check-delete-apps user app))
+      (apps/permanently-delete-apps user {:app_ids [(:id app)]}))))
+
+(deftest test-public-app-ratings
+  (let [user (get-user :testde1)]
+    (sql/delete :app_documentation (sql/where {:app_id (:id test-app)}))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-rating user)))
+    (is (thrown-with-msg? ExceptionInfo #"private app" (check-unrating user)))
+    (apps/make-app-public user test-app)
+    (is (check-rating user))
+    (is (check-unrating user))))
+
+(defn has-permission? [rt rn st sn level]
+  (let [client (config/permissions-client)]
+    (seq (:permissions (pc/get-subject-permissions-for-resource client st sn rt rn false level)))))
+
+(deftest test-public-app-permissions
+  (let [{username :shortUsername :as user} (get-user :testde1)]
+    (sql/delete :app_documentation (sql/where {:app_id (:id test-app)}))
+    (is (has-permission? "app" (:id test-app) "user" username "own"))
+    (is (not (has-permission? "app" (:id test-app) "group" (ipg/grouper-user-group-id) "read")))
+    (apps/make-app-public user test-app)
+    (is (not (has-permission? "app" (:id test-app) "user" username "own")))
+    (is (has-permission? "app" (:id test-app) "group" (ipg/grouper-user-group-id) "read"))))
+
+(defn share-app [sharer sharee app-id level]
+  (apps/share-apps sharer [{:user (:shortUsername sharee)
+                            :apps [{:app_id     app-id
+                                    :permission level}]}]))
+
+(deftest test-sharing
+  (let [{testde1-username :shortUsername :as testde1} (get-user :testde1)
+        {testde2-username :shortUsername :as testde2} (get-user :testde2)
+        {[user-response :as responses] :sharing}      (share-app testde1 testde2 (:id test-app) "own")]
+    (is (= 1 (count responses)))
+    (is (= testde2-username (:user user-response)))
+    (is (= 1 (count (:apps user-response))))
+    (is (= (:id test-app) (uuidify (-> user-response :apps first :app_id))))
+    (is (= (:name test-app) (-> user-response :apps first :app_name)))
+    (is (= "own" (-> user-response :apps first :permission)))
+    (is (true? (-> user-response :apps first :success)))
+    (is (has-permission? "app" (:id test-app) "user" testde2-username "own"))))
+
+(deftest test-sharing-no-privs
+  (let [{testde2-username :shortUsername :as testde2} (get-user :testde2)
+        {testde3-username :shortUsername :as testde3} (get-user :testde3)
+        {[user-response :as responses] :sharing}      (share-app testde2 testde3 (:id test-app) "own")]
+    (is (= 1 (count responses)))
+    (is (= testde3-username (:user user-response)))
+    (is (= 1 (count (:apps user-response))))
+    (is (= (:id test-app) (uuidify (-> user-response :apps first :app_id))))
+    (is (= (:name test-app) (-> user-response :apps first :app_name)))
+    (is (= "own" (-> user-response :apps first :permission)))
+    (is (false? (-> user-response :apps first :success)))
+    (is (re-find #"insufficient privileges" (-> user-response :apps first :error :reason)))
+    (is (not (has-permission? "app" (:id test-app) "user" testde2-username "own")))))
+
+(deftest test-sharing-write-privs
+  (let [{testde2-username :shortUsername :as testde2} (get-user :testde2)
+        {testde3-username :shortUsername :as testde3} (get-user :testde3)]
+    (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" testde2-username "write")
+    (let [{[user-response :as responses] :sharing} (share-app testde2 testde3 (:id test-app) "own")]
+      (is (= 1 (count responses)))
+      (is (= testde3-username (:user user-response)))
+      (is (= 1 (count (:apps user-response))))
+      (is (= (:id test-app) (uuidify (-> user-response :apps first :app_id))))
+      (is (= (:name test-app) (-> user-response :apps first :app_name)))
+      (is (= "own" (-> user-response :apps first :permission)))
+      (is (false? (-> user-response :apps first :success)))
+      (is (re-find #"insufficient privileges" (-> user-response :apps first :error :reason)))
+      (is (not (has-permission? "app" (:id test-app) "user" testde2-username "own"))))))
+
+(deftest test-sharing-non-existent-app
+  (let [{testde1-username :shortUsername :as testde1} (get-user :testde1)
+        {testde2-username :shortUsername :as testde2} (get-user :testde2)]
+    (let [{[user-response :as responses] :sharing} (share-app testde1 testde2 (uuid) "own")]
+      (is (= 1 (count responses)))
+      (is (= testde2-username (:user user-response)))
+      (is (= 1 (count (:apps user-response))))
+      (is (= "own" (-> user-response :apps first :permission)))
+      (is (false? (-> user-response :apps first :success)))
+      (is (re-find #"does not exist" (-> user-response :apps first :error :reason))))))
+
+(deftest test-permission-listings
+  (let [{testde1-username :shortUsername :as testde1} (get-user :testde1)
+        {testde2-username :shortUsername :as testde2} (get-user :testde2)]
+    (is (empty? (-> (apps/list-app-permissions testde1 [(:id test-app)]) :apps first :permissions)))
+    (pc/grant-permission (config/permissions-client) "app" (:id test-app) "user" testde2-username "write")
+    (let [perms (-> (apps/list-app-permissions testde1 [(:id test-app)]) :apps first :permissions)]
+      (is (= 1 (count perms)))
+      (is (= testde2-username (-> perms first :user)))
+      (is (= "write" (-> perms first :permission))))
+    (pc/revoke-permission (config/permissions-client) "app" (:id test-app) "user" testde2-username)))
